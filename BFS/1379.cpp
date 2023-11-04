04d4a277e636295f15d1667f8bee4e13e8d405da
ksks2012 <st890751999@gmail.com>
2023-11-04 17:11:13 +0800

    feat(1379): Find a Corresponding Node of a Binary Tree in a Clone of That Tree
    - DFS
    - BFS

---
+++ BFS/1379.cpp
@@ -0,0 +1,65 @@
+#include "../include.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// DFS
+// Time: O(n), Space: O(n)
+class Solution {
+public:
+    void DFS(TreeNode* cloned, int target, TreeNode **ans) {
+        if(cloned == nullptr) {
+            return;
+        }
+        if(cloned->val == target) {
+            *ans = cloned;
+            return;
+        }
+        DFS(cloned->left, target, ans);
+        DFS(cloned->right, target, ans);
+    }
+
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        TreeNode* ans = nullptr;
+        DFS(cloned, target->val, &ans);
+        return ans;
+    }
+};
+
+// Time: O(n), Space: O(n)
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+		// check null
+		if(original == NULL)
+			return NULL;
+
+		queue<TreeNode*> q;
+		q.push(cloned);
+
+		while (!q.empty())
+		{
+			int size = q.size();
+			// cout << size << endl;
+			for (int i = 0; i < size; i++) {
+				TreeNode* cur = q.front();
+				q.pop();
+				if(cur->val == target->val)
+					return cur;
+				if(cur->left)
+					q.push(cur->left);
+				if (cur->right)
+					q.push(cur->right);
+			}
+		}
+
+		return NULL;
+    }
+};
---
+++ include.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <iostream>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <numeric>
+#include <bitset> 
+#include <queue>
+
+using namespace std;
+
+vector<int> way = {1, 0, -1, 0, 1};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
