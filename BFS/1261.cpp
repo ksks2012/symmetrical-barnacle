2af4fee63f5ba8f17e14676b45f143b39f2923de
ksks2012 <st890751999@gmail.com>
2023-11-08 14:45:26 +0800

    feat(1261): Find Elements in a Contaminated Binary Tree
    - Solution with DFS
    - Solution with set
    - Solution with bitset

---
+++ BFS/1261.cpp
@@ -0,0 +1,125 @@
+#include "../include.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    TreeNode* root;
+
+    void recovery(TreeNode* root) {
+        if(root == nullptr) return;
+
+        if(root->left != nullptr)
+            root->left->val = root->val * 2 + 1;
+        if(root->right != nullptr)
+            root->right->val = root->val * 2 + 2;
+
+        recovery(root->left);
+        recovery(root->right);
+    }
+
+    FindElements(TreeNode* root) {
+        this->root = root;
+        root->val = 0;
+        recovery(root);
+    }
+
+    bool DFS(TreeNode* root, int target) {
+        if(root == nullptr) 
+            return false;
+
+        if(root->val == target)
+            return true;
+
+        return DFS(root->left, target) || DFS(root->right, target);
+    }
+    
+    bool find(int target) {
+        return DFS(root, target);
+    }
+};
+
+// with set in find
+class FindElements {
+public:
+    TreeNode* root;
+    set<int> val;
+
+    void recovery(TreeNode* root) {
+        if(root == nullptr) return;
+
+        if(root->left != nullptr) {
+            root->left->val = root->val * 2 + 1;
+            val.insert(root->left->val);
+        }
+            
+        if(root->right != nullptr) {
+            root->right->val = root->val * 2 + 2;
+            val.insert(root->right->val);
+        }
+            
+
+        recovery(root->left);
+        recovery(root->right);
+    }
+
+    FindElements(TreeNode* root) {
+        this->root = root;
+        root->val = 0;
+        val.insert(root->val);
+        recovery(root);
+    }
+
+    bool find(int target) {
+        return val.count(target);
+    }
+};
+
+// bitset in find
+class FindElements {
+public:
+    TreeNode* root;
+    bitset<10000000> tmp;
+    void recovery(TreeNode* root) {
+        if(root == nullptr) return;
+        
+        this->tmp.set(root->val);
+
+        if(root->left != nullptr) {
+            root->left->val = root->val * 2 + 1;
+        }
+            
+        if(root->right != nullptr) {
+            root->right->val = root->val * 2 + 2;
+        }
+            
+
+        recovery(root->left);
+        recovery(root->right);
+    }
+
+    FindElements(TreeNode* root) {
+        this->root = root;
+        root->val = 0;
+        recovery(root);
+    }
+
+    bool find(int target) {
+        return this->tmp.test(target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
