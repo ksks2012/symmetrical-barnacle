feefd9d5f01f9d9c8df2d65e36638b7078e9e7da
ksks2012 <st890751999@gmail.com>
2023-11-10 07:44:38 +0800

    feat(1302): Add recursive solution
    - 2 DFS
    - DFS with map record

--- BFS/1302.cpp
+++ BFS/1302.cpp
@@ -38,3 +38,48 @@ public:
         return ans;
     }
 };
+
+// 2 DFS
+class Solution {
+public:
+    int max_depth;
+    int deepestLeavesSum(TreeNode* root) {
+        max_depth = find_deep(root);
+        return sum_deep(root, 0);
+    }
+    
+    int sum_deep(TreeNode* root, int depth) {
+        if(!root) return 0;
+        if(depth==max_depth-1) return root->val;
+        return sum_deep(root->left, depth+1) +
+            sum_deep(root->right, depth+1);
+    
+    }
+
+    int find_deep(TreeNode* root) {
+        if(!root) return 0;
+        return max(1 + find_deep(root->left), 1 + find_deep(root->right));
+    }
+};
+
+class Solution {
+public:
+    map<int, int> layer_sum;
+    int deepestLeavesSum(TreeNode* root) {
+        sum_layer(root, 0);
+
+        int ans = 0;
+        for(auto m : layer_sum) {
+            ans = m.second;
+        }
+        return ans;
+    }
+
+    void sum_layer(TreeNode* root, int level) {
+        if(root == nullptr) 
+            return;
+        layer_sum[level] += root->val;
+        sum_layer(root->left, level + 1);
+        sum_layer(root->right, level + 1);        
+    }
+};
