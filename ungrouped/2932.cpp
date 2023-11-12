17b8a90b4f32e24f4b62447b874ad00558fa64fb
ksks2012 <st890751999@gmail.com>
2023-11-12 18:43:30 +0800

    feat(2932): Maximum Strong Pair XOR I

---
+++ ungrouped/2932.cpp
@@ -0,0 +1,20 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int ans = 0;
+        int m = nums.size();
+        
+        int max_num = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < m; j++) {
+                int tmp = nums[i] ^ nums[j];
+                if(abs(nums[i] - nums[j]) <= min(nums[i], nums[j]))
+                max_num = max(max_num, tmp);
+            }
+        }
+
+        return max_num;        
+    }
+};
