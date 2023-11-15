5bfc2a02ad1da2f19f773ee85d074dbde5a293c5
ksks2012 <st890751999@gmail.com>
2023-11-15 22:12:21 +0800

    feat(1319): Number of Operations to Make Network Connected

---
+++ BFS/1319.cpp
@@ -0,0 +1,36 @@
+#include "../include.h"
+
+class Solution {
+    void DFS(vector<vector<int>> adj, vector<bool> &visited, int src) {
+        visited[src] = true;
+        for(int i : adj[src]) {
+            if(!visited[i]) {
+                DFS(adj, visited, i);
+            }
+        }
+    }
+public:
+    int makeConnected(int n, vector<vector<int>>& arr) {
+        int len = arr.size();
+        if(len < n - 1) 
+            return -1;
+
+        vector<vector<int>> adj(n);
+        for(auto v : arr) {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+
+        vector<bool> visited(n, false);
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                DFS(adj, visited, i);
+                ans++;
+            }
+        }
+        return ans - 1;
+    }
+};
+
+
