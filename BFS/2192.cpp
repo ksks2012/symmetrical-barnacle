c2426489a3dd48048eb512043e655538981184ba
ksks2012 <st890751999@gmail.com>
2023-11-24 04:12:10 +0800

    feat(2192): Add BFS method

--- BFS/2192.cpp
+++ BFS/2192.cpp
@@ -3,7 +3,6 @@
 class Solution {
 public:
     void DFS(vector<vector<int>> &tmp, vector<vector<int>> &ans, int cur, int start) {
-        cout << cur << " " << start << endl;
         for(auto i : tmp[cur]) {
             if(ans[i].empty() || ans[i].back() != start) {
                 ans[i].push_back(start);
@@ -21,6 +20,35 @@ public:
         vector<vector<int>> ans(n);
         for(int i = 0; i < n; i++) {
             DFS(tmp, ans, i, i);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ans (n);
+        vector<vector<int>> tmp (n, vector<int>{}); //store the ancestors
+        for (auto e : edges)
+            tmp[e[1]].push_back(e[0]);
+        for (int i = 0; i < n; i++) {
+            vector<bool> visit (n, 0); //visited ancestor in this bfs search path
+            queue<int> q;
+            q.push(i);
+            while (!q.empty()) {
+                if (!visit[q.front()]) {
+                    visit[q.front()] = 1;
+                    for (auto n : tmp[q.front()]) //find grand parents
+                        if (!visit[n])
+                            q.push(n);
+                }
+                q.pop();
+            }
+            visit[i] = 0; //excluding descendant itself
+            for (int anc = 0; anc < n; anc++)
+                if (visit[anc])
+                    ans[i].push_back(anc);
         }
         return ans;
     }
