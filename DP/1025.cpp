9ba5389a7587c04109c7cf0a8a33c205cdc04613
ksks2012 <st890751999@gmail.com>
2023-11-27 07:24:58 +0800

    feat(1025): Divisor Game

---
+++ DP/1025.cpp
@@ -0,0 +1,22 @@
+#include "include.h"
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n % 2 == 0;
+    }
+};
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<int> dp(n + 1, 0);
+        for(int i = 2;i < n + 1; i++){
+            for(int j = 1; j < i; j++){
+                if(i % j == 0)
+                    dp[i] = dp[i] | !dp[i - j];  //(!dp[i - j] checks if i - j is a losing position)
+            }
+        }
+        return dp[n];
+    }
+};
