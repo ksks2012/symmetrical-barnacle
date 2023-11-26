164e59e5537a985f1d54ff1c5bbc59ce9ab26222
ksks2012 <st890751999@gmail.com>
2023-11-27 07:52:54 +0800

    feat(746): Min Cost Climbing Stairs

---
+++ DP/746.cpp
@@ -0,0 +1,32 @@
+#include "include.h"
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int m = cost.size();
+
+        vector<int> dp(m + 1, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < m + 1; i++) {
+            int tmp = 0;
+            if(i < m)
+                tmp = cost[i];
+            dp[i] = tmp + min(dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[m];
+    }
+};
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        for(int i = 2; i < n; i++) {
+            cost[i]+=min(cost[i - 1], cost[i - 2]);
+        }
+        return min(cost[n - 1], cost[n - 2]);
+    }
+};
