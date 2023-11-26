5a20e42b05e805980c85541913f473270d2ac084
ksks2012 <st890751999@gmail.com>
2023-11-26 09:43:29 +0800

    feat(2944): Update

--- ungrouped/2944.cpp
+++ ungrouped/2944.cpp
@@ -72,10 +72,27 @@ public:
             // buy i-th
             dp[i + 1][1] = min(dp[i][0] ,dp[i][1]) + prices[i];
             // not buy i-th
-            for(int j = i; j + j >= i  + 1; j --){
+            for(int j = i; j + j >= i + 1; j--){
                 dp[i + 1][0] = min(dp[i + 1][0] , dp[j][1]);
             }
         }
         return min(dp[n][0], dp[n][1]);
+    }
+};
+
+class Solution {
+public:
+    int minimumCoins(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n] = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            int from = i + 1;
+            int to = from + i + 1;
+            for(int j = from; j < min(to, n) + 1; j++) {
+                dp[i] = min(dp[i], dp[j] + prices[i]);
+            }
+        }
+        return dp[0];
     }
 };
