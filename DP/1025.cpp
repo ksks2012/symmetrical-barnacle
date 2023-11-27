387adbbf58d5b68ed4f4a09b5987d1ebfb268076
ksks2012 <st890751999@gmail.com>
2023-11-27 23:03:46 +0800

    fix(1025): Format

--- DP/1025.cpp
+++ DP/1025.cpp
@@ -11,7 +11,7 @@ class Solution {
 public:
     bool divisorGame(int n) {
         vector<int> dp(n + 1, 0);
-        for(int i = 2;i < n + 1; i++){
+        for(int i = 2; i < n + 1; i++){
             for(int j = 1; j < i; j++){
                 if(i % j == 0)
                     dp[i] = dp[i] | !dp[i - j];  //(!dp[i - j] checks if i - j is a losing position)
