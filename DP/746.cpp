79d2d32b064817bd4a33cdd14c116da456ae2a30
ksks2012 <st890751999@gmail.com>
2023-11-27 23:06:23 +0800

    fix(746): Format

--- DP/746.cpp
+++ DP/746.cpp
@@ -25,7 +25,7 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         for(int i = 2; i < n; i++) {
-            cost[i]+=min(cost[i - 1], cost[i - 2]);
+            cost[i] += min(cost[i - 1], cost[i - 2]);
         }
         return min(cost[n - 1], cost[n - 2]);
     }
