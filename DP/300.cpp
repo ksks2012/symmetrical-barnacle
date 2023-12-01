bc8705e5860464cbc348716adf6ce89e26ae69f2
ksks2012 <st890751999@gmail.com>
2023-12-01 09:12:18 +0800

    feat(300): Format

--- DP/300.cpp
+++ DP/300.cpp
@@ -5,18 +5,18 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int>LIS(n+1, 1);
+        vector<int> dp(n + 1, 1);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j])
-                    LIS[i] = max(LIS[i], 1 + LIS[j]);
+                    dp[i] = max(dp[i], 1 + dp[j]);
             }
         }
 
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            ans = max(ans, LIS[i]);
+            ans = max(ans, dp[i]);
         }
 
         return ans;
