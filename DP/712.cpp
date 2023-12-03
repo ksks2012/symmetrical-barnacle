9dd0b256f900beb60c6c56562a00cf223d80027c
ksks2012 <st890751999@gmail.com>
2023-12-04 00:12:04 +0800

    feat(712): Update

--- DP/712.cpp
+++ DP/712.cpp
@@ -29,3 +29,69 @@ public:
         return dp[0][0];
     }
 };
+
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] + s1[i - 1];
+        }
+
+        for(int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] + s2[j - 1];
+        }
+            
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(dp[i - 1][j] + s1[i - 1], dp[i][j - 1] + s2[j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
+// MLE
+class Solution {
+public:
+    int computeCost(map<pair<int, int>, int> &storage, string s1, string s2, int i, int j) {
+        if (i < 0 && j < 0) {
+            return 0;
+        }
+        pair<int, int> key = pair<int, int>(i, j);
+
+        // If already computed, then return the result from the hash map
+        if(storage.find(key) != storage.end()) {
+            return storage[key];
+        }
+
+        // If any one string is empty, delete all characters of the other string
+        if (i < 0) {
+            storage[key] = s2[j] + computeCost(storage, s1, s2, i, j - 1);
+            return storage[key];
+        }
+        if (j < 0) {
+            storage[key] = s1[i] + computeCost(storage, s1, s2, i - 1, j);
+            return storage[key];
+        }
+        // Call sub-problem depending on s1[i] and s2[j]
+        // Save the computed result.
+        if (s1[i] == s2[j]) {
+            storage[key] = computeCost(storage, s1, s2, i - 1, j - 1);
+        } else {
+            storage[key] = min(s1[i] + computeCost(storage, s1, s2, i - 1, j), s2[j] + computeCost(storage, s1, s2, i, j - 1));
+        }
+        return storage[key];
+    }   
+
+    int minimumDeleteSum(string s1, string s2) {
+        map<pair<int, int>, int> storage;
+        return computeCost(storage, s1, s2, s1.length() - 1, s2.length() - 1);
+    }
+};
---
+++ ungrouped/2953.cpp
@@ -0,0 +1,92 @@
+#include "include.h"
+
+class Solution {
+public:
+    bool checkfreq(unordered_map<char, int>& freq, int k) {
+        for (auto& pair : freq) {
+            if (pair.second != k && pair.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int countCompleteSubstrings(string word, int k) {
+        vector<pair<int, int>> splits;
+        int ans = 0;
+        int len = word.size();
+        for(int i = 0; i < len; i++) {
+            int temp = i;
+            if(i < len - 1) {
+                while(i < len - 1 && abs(word[i+1] - word[i]) <= 2) {
+                    i++;
+                }
+                splits.push_back(make_pair(temp, i + 1));
+            }
+            else {
+                splits.push_back(make_pair(len - 1, len));
+            }
+        }
+        for(auto x: splits) {
+            int start = x.first, end = x.second, len = end - start + 1;
+            for(int windowsize = k; windowsize <= min(len, 26 * k); windowsize += k) {
+                int windowstart = start, windowend = windowstart + windowsize - 1;
+                unordered_map<char, int> freq;
+                for(int j = windowstart; j <= windowend; j++) {
+                    freq[word[j]]++;
+                }
+                while (windowend < end) {
+                    if (checkfreq(freq, k)) {
+                        ans++;
+                    }
+                    freq[word[windowstart]]--;
+                    windowstart++;
+                    windowend++;
+                    freq[word[windowend]]++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int countCompleteSubstrings(string word, int K) {
+        int n = word.size();
+
+        // f[i][c] length i prefix has how many char c
+        int f[n + 1][26];
+        for(int i = 0; i < 26; i++) {
+            f[0][i] = 0;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < 26; j++) {
+                f[i][j] = f[i - 1][j];
+            }
+            f[i][word[i - 1] - 'a']++;
+        }
+
+        long long ans = 0;
+        // last i, section start at j
+        for(int i = 1, j = 0; i <= n; i++) {
+            if(i > 1 && abs(word[i - 1] - word[i - 2]) > 2)
+                j = i - 1;
+            
+            for(int fac = 1, L = i - K; fac <= 26 && L >= j; fac++, L-= K) {
+                bool ok = true;
+                // check char nums
+                for(int k = 0; k < 26; k++) {
+                    int det = f[i][k] - f[L][k];
+                    if(det != 0 && det != K) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if(ok)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
