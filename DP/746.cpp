74094c768a5d163d3ce96c5c1d0e78118ca08a56
ksks2012 <st890751999@gmail.com>
2023-12-04 16:01:59 +0800

    feat(746): Update

--- DP/746.cpp
+++ DP/746.cpp
@@ -30,3 +30,41 @@ public:
         return min(cost[n - 1], cost[n - 2]);
     }
 };
+
+// TLE
+class Solution {
+public:
+    int DFS(vector<int>& cost, int cur) {
+        if(cur < 0)
+            return 0;
+        
+        return cost[cur] + min(DFS(cost, cur - 1), DFS(cost, cur - 2));
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        
+        return min(DFS(cost, n - 1), DFS(cost, n - 2));
+    }
+};
+
+class Solution {
+public:
+    int DFS(vector<int>& cost, vector<int>& memo, int cur) {
+        if(cur < 0)
+            return 0;
+        if(memo[cur] != -1)
+            return memo[cur];
+
+        memo[cur] = cost[cur] + min(DFS(cost, memo, cur - 1), DFS(cost, memo, cur - 2));
+        
+        return memo[cur];
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> memo(n + 1, -1);
+        
+        return min(DFS(cost, memo, n - 1), DFS(cost, memo, n - 2));
+    }
+};
