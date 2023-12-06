8af977fbf1f2619c2cf44df01770375cd988c843
ksks2012 <st890751999@gmail.com>
2023-12-07 06:53:03 +0800

    feat(279): Update steps

--- DP/279.cpp
+++ DP/279.cpp
@@ -1,18 +1,87 @@
 #include "include.h"
 
+// TLE
+class Solution {
+public:
+    int DFS(int target, int i) {
+        if(i <= 0) {
+            if(target == 0)
+                return 0;
+            else
+                return INT_MAX / 2; 
+        }
+        if(i * i > target) {
+            return DFS(target, i - 1);
+        }
+        return min(DFS(target, i - 1), DFS(target - i * i, i) + 1);
+    }
+
+    int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        }
+        
+        int cur = n / 2;
+        int ans = DFS(n, cur);
+        if(ans > INT_MAX / 2) {
+            return 0;
+        }
+        return ans;
+    }
+};
+
+// MLE
+class Solution {
+public:
+    int DFS(int target, int i, vector<vector<int>>& memo) {
+        if(i <= 0) {
+            if(target == 0)
+                return 0;
+            else
+                return INT_MAX / 2; 
+        }
+        int &res = memo[target][i];
+        if(res != -1) {
+            return res;
+        }
+        if(i * i > target) {
+            return res = DFS(target, i - 1, memo);
+        }
+        return res = min(DFS(target, i - 1, memo), DFS(target - i * i, i, memo) + 1);
+    }
+
+    int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        }
+        
+        int cur = n / 2;
+        vector<vector<int>> memo(n + 1, vector<int>(cur + 1, -1));
+        int ans = DFS(n, cur, memo);
+        if(ans > INT_MAX / 2) {
+            return 0;
+        }
+        return ans;
+    }
+};
+
 // DP
 class Solution {
 public:
     int numSquares(int n) {
-		vector<int> dp(n + 1, INT_MAX);
-		dp[0] = 0;
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
 
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j * j <= i; j++) {
-				dp[i] = min(dp[i], dp[i - j * j] + 1);
-			}
-		}
-		return dp[n];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 };
 
