e0941dfd2a541530d6d2cd8ba873edac30f88465
ksks2012 <st890751999@gmail.com>
2023-12-07 04:50:37 +0800

    feat(416): Update steps

--- DP/416.cpp
+++ DP/416.cpp
@@ -1,5 +1,73 @@
 #include "include.h"
 
+// TLE
+class Solution {
+public:
+    bool DFS(vector<int>& nums, int target, int cur_idx) {
+        if(cur_idx < 0) {
+            if(target == 0)
+                return true;
+            else
+                return false;
+        }
+        if(target < nums[cur_idx])
+            return DFS(nums, target, cur_idx - 1);
+        return DFS(nums, target, cur_idx - 1) || DFS(nums, target - nums[cur_idx], cur_idx - 1);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        // find sum of all array elements
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
+
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (sum % 2 != 0) 
+            return false;
+
+        int target = sum / 2;
+        return DFS(nums, target, n - 1);
+    }
+};
+
+// MLE
+class Solution {
+public:
+    bool DFS(vector<int>& nums, int target, int cur_idx, vector<vector<int>> memo) {
+        if(cur_idx < 0) {
+            if(target == 0)
+                return true;
+            else
+                return false;
+        }
+        int &res = memo[target][cur_idx];
+        if(res != -1)
+            return res;
+        if(target < nums[cur_idx])
+            return res = DFS(nums, target, cur_idx - 1, memo);
+        return res = DFS(nums, target, cur_idx - 1, memo) || DFS(nums, target - nums[cur_idx], cur_idx - 1, memo);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        // find sum of all array elements
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
+
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (sum % 2 != 0) 
+            return false;
+
+        int target = sum / 2;
+        vector<vector<int>> memo(target + 1, vector<int>(n + 1, -1));
+        return DFS(nums, target, n - 1, memo);
+    }
+};
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
