fd50993d7bc3c5f14f5c20d05bd0adf41598e431
ksks2012 <st890751999@gmail.com>
2023-12-08 04:50:06 +0800

    feat(72): Update

--- DP/72.cpp
+++ DP/72.cpp
@@ -1,5 +1,122 @@
 #include "include.h"
 
+// DFS
+class Solution {
+public:
+    string s;
+    string t;
+    int DFS(int i, int j) {
+        if(i < 0)
+            return j + 1;
+        if(j < 0)
+            return i + 1;
+        
+        if(s[i] == t[j])
+            return DFS(i - 1, j - 1);
+        return min(DFS(i - 1, j), min(DFS(i, j - 1), DFS(i - 1, j - 1))) + 1; 
+    }
+    
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        s = word1;
+        t = word2;
+    
+        return DFS(m - 1, n - 1);
+    }
+};
+
+// DFS with memo
+class Solution {
+public:
+    string s;
+    string t;
+    int DFS(int i, int j, vector<vector<int>> &memo) {
+        if(i < 0)
+            return j + 1;
+        if(j < 0)
+            return i + 1;
+        
+        int &res = memo[i][j];
+        if(res != -1)
+            return res;
+        
+        if(s[i] == t[j])
+            return res = DFS(i - 1, j - 1, memo);
+        return res = min(DFS(i - 1, j, memo), min(DFS(i, j - 1, memo), DFS(i - 1, j - 1, memo))) + 1; 
+    }
+    
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        s = word1;
+        t = word2;
+
+        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
+        return DFS(m - 1, n - 1, memo);
+    }
+};
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(word1[i] == word2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = min(dp[i][j + 1], min(dp[i + 1][j], dp[i][j])) + 1; 
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+// Space Optimization
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<int> dp(n + 1, 0);
+        for (int j = 0; j <= n; j++) {
+            dp[j] = j;
+        }
+        
+        for(int i = 0; i < m; i++) {
+            int prev = dp[0];
+            dp[0]++;
+            for(int j = 0; j < n; j++) {
+                int tmp = dp[j + 1];
+                if(word1[i] == word2[j]) {
+                    dp[j + 1] = prev;
+                } else {
+                    dp[j + 1] = min(dp[j + 1], min(dp[j], prev)) + 1; 
+                }
+                prev = tmp;
+            }
+        }
+        return dp[n];
+    }
+};
+
+
 class Solution {
 public:
     int minDistance(string word1, string word2) {
