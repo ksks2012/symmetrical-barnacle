be6b7b7290f47f50b45880add14785a5119df3e1
ksks2012 <st890751999@gmail.com>
2023-12-08 23:30:48 +0800

    feat(712): Update

--- DP/712.cpp
+++ DP/712.cpp
@@ -1,5 +1,188 @@
 #include "include.h"
 
+// TLE
+class Solution {
+public:
+    string s;
+    string p;
+    int DFS(int i, int j) {
+        if(i < 0) {
+            int tmp = 0;
+            for(int k = j; k >= 0; k--) {
+                tmp += p[k];
+            }
+            return tmp;
+            return 0;
+        }
+        if(j < 0) {
+            int tmp = 0;
+            for(int k = i; k >= 0; k--) {
+                tmp += s[k];
+            }
+            return tmp;
+            return 0;
+        }
+        
+        if(s[i] == p[j])
+            return DFS(i - 1, j - 1);
+
+        return min(DFS(i - 1, j - 1) + s[i] + p[j], min(DFS(i, j - 1) + p[j], DFS(i - 1, j) + s[i]));
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        int ans = 1e9;
+
+        int m = s1.size();
+        int n = s2.size();
+
+        s = s1;
+        p = s2;
+
+        ans = min(DFS(m - 1, n - 1), ans);
+        return ans;
+    }
+};
+
+// DFS with memo
+class Solution {
+public:
+    string s;
+    string p;
+    int DFS(int i, int j, vector<vector<int>>& memo) {
+        if(i < 0) {
+            int tmp = 0;
+            for(int k = j; k >= 0; k--) {
+                tmp += p[k];
+            }
+            return tmp;
+            return 0;
+        }
+        if(j < 0) {
+            int tmp = 0;
+            for(int k = i; k >= 0; k--) {
+                tmp += s[k];
+            }
+            return tmp;
+            return 0;
+        }
+
+        int &res = memo[i][j];
+        if(res != -1)
+            return res;
+        
+        if(s[i] == p[j])
+            return res = DFS(i - 1, j - 1, memo);
+
+        return res = min(DFS(i - 1, j - 1, memo) + s[i] + p[j], min(DFS(i, j - 1, memo) + p[j], DFS(i - 1, j, memo) + s[i]));
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        int ans = 1e9;
+
+        int m = s1.size();
+        int n = s2.size();
+
+        s = s1;
+        p = s2;
+        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
+        ans = min(DFS(m - 1, n - 1, memo), ans);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int minimumDeleteSum(string s, string t) {
+        int ans = 1e9;
+
+        int m = s.size();
+        int n = t.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < m; i++) {
+            dp[i + 1][0] = dp[i][0] + s[i];
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j + 1] = dp[0][j] + t[j];
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(s[i] == t[j]) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = min(dp[i][j] + s[i] + t[j], min(dp[i + 1][j] + t[j], dp[i][j + 1] + s[i]));
+                }
+            }
+        }
+         
+        return dp[m][n];
+    }
+};
+
+class Solution {
+public:
+    int minimumDeleteSum(string s, string t) {
+        int ans = 1e9;
+
+        int m = s.size();
+        int n = t.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < m; i++) {
+            dp[i + 1][0] = dp[i][0] + s[i];
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j + 1] = dp[0][j] + t[j];
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(s[i] == t[j]) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = min(dp[i][j] + s[i] + t[j], min(dp[i + 1][j] + t[j], dp[i][j + 1] + s[i]));
+                }
+            }
+        }
+         
+        return dp[m][n];
+    }
+};
+
+// Space Optimization
+class Solution {
+public:
+    int minimumDeleteSum(string s, string t) {
+        int ans = 1e9;
+
+        int m = s.size();
+        int n = t.size();
+
+        vector<int> dp(n + 1, 0);
+
+        for (int j = 0; j < n; j++) {
+            dp[j + 1] = dp[j] + t[j];
+        }
+
+        for(int i = 0; i < m; i++) {
+            int prev = dp[0];
+            dp[0] = dp[0] + s[i];
+            for(int j = 0; j < n; j++) {
+                int tmp = dp[j + 1];
+                if(s[i] == t[j]) {
+                    dp[j + 1] = prev;
+                } else {
+                    dp[j + 1] = min(prev + s[i] + t[j], min(dp[j] + t[j], dp[j + 1] + s[i]));
+                }
+                prev = tmp;
+            }
+        }
+         
+        return dp[n];
+    }
+};
+
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
