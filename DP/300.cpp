0fa093ec19579bfd2d9d0ce4d72f4dade0ba96f5
ksks2012 <st890751999@gmail.com>
2023-12-11 04:28:28 +0800

    feat(300): Update
    - Add steps of DFS
    - Add Time Optimization Solution

--- DP/300.cpp
+++ DP/300.cpp
@@ -1,5 +1,63 @@
 #include "../include.h"
 
+// TLE
+class Solution {
+public:
+    vector<int> nums;
+    int n;
+    int DFS(int i) {
+        int res = 0;
+        for(int j = 0; j < i; j++) {
+            if(nums[i] > nums[j]) {
+                res = max(res, DFS(j));
+            }
+        }
+        return res + 1;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        n = nums.size();
+        this->nums = nums;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, DFS(i));
+        }
+        
+        return ans;
+    }
+};
+
+// DFS with memory
+class Solution {
+public:
+    vector<int> nums;
+    int n;
+    int DFS(int i, vector<int> &memo) {
+        int &tmp = memo[i];
+        if(tmp != -1)
+            return tmp;
+        int res = 0;
+        for(int j = 0; j < i; j++) {
+            if(nums[i] > nums[j]) {
+                res = max(res, DFS(j, memo));
+            }
+        }
+        return tmp = res + 1;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        n = nums.size();
+        this->nums = nums;
+        int ans = 0;
+        vector<int> memo(n + 1, -1);
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, DFS(i, memo));
+        }
+        
+        return ans;
+    }
+};
+
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -14,11 +72,26 @@ public:
             }
         }
 
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, dp[i]);
-        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
 
-        return ans;
+// Time Optimization O(N * logN)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> tmp;
+
+        for(auto num : nums) {
+            auto it = lower_bound(tmp.begin(), tmp.end(), num);
+            if(it == tmp.end()) {
+                tmp.push_back(num);
+            } else {
+                *it = num;
+            }
+        }
+        return tmp.size();
     }
 };
