93c111cf0383e11360ac8ee1bee0c726fefc244b
ksks2012 <st890751999@gmail.com>
2023-12-10 12:08:35 +0800

    feat(2963): Count Subarrays Where Max Element Appears at Least K Times

---
+++ ungrouped/2963.cpp
@@ -0,0 +1,29 @@
+#include "../include.h"
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long result = 0;
+        int mo = *max_element(nums.begin(), nums.end());
+
+        vector<int> tmp;
+
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] == mo) {
+                tmp.push_back(i);
+            }
+        }
+        
+        if(tmp.size() < k)
+            return 0;
+
+        long long tag = 0;
+        for(int i = 0; i < tmp.size() - k + 1; ++i) {
+            if(i + k - 1 < n)
+                result += (tmp[i] - tag + 1) * (n - tmp[i + k - 1]);
+            tag = tmp[i] + 1;
+        }
+        return result;
+    }
+};
