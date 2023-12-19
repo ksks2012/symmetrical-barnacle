56fe2ad2984db774e44ff54a5827f53b976602e6
ksks2012 <st890751999@gmail.com>
2023-12-20 04:52:21 +0800

    feat(1000): Update DFS with memory

--- DP/1000.cpp
+++ DP/1000.cpp
@@ -3,7 +3,7 @@
 // DFS
 class Solution {
 public:
-    int m, n;
+    int n;
     int k;
     vector<int> s;
     int DFS(int i, int j, int p) {
@@ -27,5 +27,39 @@ public:
             s[i + 1] = s[i] + stones[i]; // Prefix sum
 
         return DFS(0, n - 1, 1);
+    }
+};
+
+// DFS with memory
+class Solution {
+public:
+    int n;
+    int k;
+    vector<int> s;
+    int DFS(int i, int j, int p, vector<vector<vector<int>>>& memo) {
+        int &res = memo[i][j][p];
+        if(res != -1)
+            return res;
+        if (p == 1) // merge to 1
+            return res = i == j ? 0 : DFS(i, j, k, memo) + s[j + 1] - s[i];
+        res = INT_MAX;
+        for (int m = i; m < j; m += k - 1) // find stones to merge
+            res = min(res, DFS(i, m, 1, memo) + DFS(m + 1, j, p - 1, memo));
+        return res;
+    }
+
+    int mergeStones(vector<int> &stones, int k) {
+        n = stones.size();
+        this->k = k;
+        if ((n - 1) % (k - 1)) // Can't be merged
+            return -1;
+
+        s = vector<int>(n + 1, 0);
+        s[0] = 0;
+        for (int i = 0; i < n; i++)
+            s[i + 1] = s[i] + stones[i]; // Prefix sum
+
+        vector<vector<vector<int>>> memo(n, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        return DFS(0, n - 1, 1, memo);
     }
 };
