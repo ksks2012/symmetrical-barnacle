0c8cdfa7602095a1eca48a087219c2a12dff605e
ksks2012 <st890751999@gmail.com>
2023-12-20 04:50:18 +0800

    feat(1000): Minimum Cost to Merge Stones

---
+++ DP/1000.cpp
@@ -0,0 +1,31 @@
+#include "include.h"
+
+// DFS
+class Solution {
+public:
+    int m, n;
+    int k;
+    vector<int> s;
+    int DFS(int i, int j, int p) {
+        if (p == 1) // merge to 1
+            return i == j ? 0 : DFS(i, j, k) + s[j + 1] - s[i];
+        int res = INT_MAX;
+        for (int m = i; m < j; m += k - 1) // find stones to merge
+            res = min(res, DFS(i, m, 1) + DFS(m + 1, j, p - 1));
+        return res;
+    }
+
+    int mergeStones(vector<int> &stones, int k) {
+        n = stones.size();
+        this->k = k;
+        if ((n - 1) % (k - 1)) // Can't be merged
+            return -1;
+
+        s = vector<int>(n + 1, 0);
+        s[0] = 0;
+        for (int i = 0; i < n; i++)
+            s[i + 1] = s[i] + stones[i]; // Prefix sum
+
+        return DFS(0, n - 1, 1);
+    }
+};
