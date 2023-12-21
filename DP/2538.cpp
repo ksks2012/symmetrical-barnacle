2a218726f6fd05c257852c38e488d516c8b3b9e4
ksks2012 <st890751999@gmail.com>
2023-12-21 19:45:57 +0800

    feat(2538): Difference Between Maximum and Minimum Price Sum

---
+++ DP/2538.cpp
@@ -0,0 +1,46 @@
+#include "include.h"
+
+#define PAIR_LL pair<long long, long long>
+class Solution {
+public:
+    long long ans = 0;
+    vector<int> price;
+    vector<vector<int>> tree;
+
+    PAIR_LL DFS(int x, int parent) {
+        long long p = price[x];
+        long long max_s1 = p;
+        long long max_s2 = 0;
+
+        for(int y: tree[x]) {
+            if(y != parent) {
+                auto [s1, s2] = DFS(y, x);
+                // Maximum leaf path sum in front + current non-leaf path sum
+                // Maximum non-leaf path sum + current leaf path sum 
+                ans = max(ans, max(max_s1 + s2, max_s2 + s1));
+
+                // x is not leaf
+                max_s1 = max(max_s1, s1 + p);
+                max_s2 = max(max_s2, s2 + p);
+            }
+        }
+        return {max_s1, max_s2};
+    }
+
+    long long maxOutput(int n, vector<vector<int>> &edges, vector<int> &price) {
+        tree = vector<vector<int>>(n);
+        // Create graph
+        for(auto e: edges) {
+            int x = e[0];
+            int y = e[1];
+            tree[x].push_back(y);
+            tree[y].push_back(x);
+        }
+
+        this->price = price;
+
+        DFS(0, -1);
+
+        return ans;
+    }
+};
