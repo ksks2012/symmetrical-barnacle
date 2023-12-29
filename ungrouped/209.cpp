5ca4c6d655e8f49254921259df6de3393dbe9b4c
ksks2012 <st890751999@gmail.com>
2023-12-29 23:47:32 +0800

    style(209)

--- ungrouped/209.cpp
+++ ungrouped/209.cpp
@@ -22,14 +22,13 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
         int left = 0;
-        // int right = 1;
 
         int ans = INT_MAX;
         int cur = 0;
-        for(int i = 0; i < n; i++) {
-            cur += nums[i];
+        for(int right = 0; right < n; right++) {
+            cur += nums[right];
             while(cur >= target) {
-                ans = min(ans, i - left + 1);
+                ans = min(ans, right - left + 1);
                 cur -= nums[left];
                 left++;
             }                
