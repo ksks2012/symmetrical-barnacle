65d7138e36ec88324ceb0977e1f419735713bd03
ksks2012 <st890751999@gmail.com>
2024-01-04 21:52:02 +0800

    feat(1423): Maximum Points You Can Obtain from Cards

---
+++ two_pointer/1423.cpp
@@ -0,0 +1,31 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int sum = 0;
+        int n = cardPoints.size();
+
+        for(auto p : cardPoints) {
+            sum += p;
+        }
+
+        int mini = INT_MAX;
+        int target = n - k;
+        int cur = 0;
+        int left = 0;
+        for(int i = 0; i < n; i++) {
+            cur += cardPoints[i];
+            // cout << i << " " << left << " " << cur << endl;
+            while(i - left + 1 > target) {
+                cur -= cardPoints[left];
+                left++;
+            }
+            if(i - left + 1 == target) {
+                mini = min(mini, cur);
+            }
+        }
+
+        return sum - mini;
+    }
+};
