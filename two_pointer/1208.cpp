bb4a32e65f53d9f5cc5b9966abfb7c46edadfc2a
ksks2012 <st890751999@gmail.com>
2024-01-08 21:29:09 +0800

    feat(1208): Get Equal Substrings Within Budget

---
+++ two_pointer/1208.cpp
@@ -0,0 +1,25 @@
+#include "include.h"
+
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        vector<int> tmp(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            tmp[i] = abs(t[i] - s[i]);
+        } 
+        
+        int left = 0;
+        int cur = 0;
+        int ans = 0;
+        for(int right = 0; right < n; right++) {
+            cur += tmp[right];
+            while(cur > maxCost) {
+                cur -= tmp[left++];
+            }
+            ans = max(ans, right -left + 1);
+        }
+        return ans;
+    }
+};
