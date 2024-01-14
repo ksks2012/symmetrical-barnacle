8714f0457be3287ed1eabd0045ce543044386443
ksks2012 <st890751999@gmail.com>
2024-01-14 15:18:30 +0800

    feat(3005): Count Elements With Maximum Frequency

---
+++ ungrouped/3005.cpp
@@ -0,0 +1,21 @@
+#include "include.h"
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> m;
+        int maxi = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+            maxi = max(maxi, m[nums[i]]);
+        }
+
+        for(auto i : m) {
+            if(i.second == maxi)
+                ans += maxi;
+        }
+
+        return ans;
+    }
+};
