7f7be7a0e27537d30df075e3cea5f0f90d201e54
ksks2012 <st890751999@gmail.com>
2024-01-17 22:02:08 +0800

    refactory(1377)

--- DP/1377.cpp
+++ DP/1377.cpp
@@ -1,5 +1,44 @@
 #include "include.h"
 
+
+// endlesscheng
+class Solution {
+public:
+    int t;
+    int target;
+    double ans = 0;
+    vector<vector<int>> graph;
+    int DFS(int x, int parent, int left_time, long long prod) {
+        if (x == target && (left_time == 0 || graph[x].size() == 1)) {
+            ans = 1.0 / prod;
+            return true;
+        }
+        if (x == target || left_time == 0) 
+            return false;
+        for (int y: graph[x])  
+            if (y != parent && DFS(y, x, left_time - 1, prod * (graph[x].size() - 1)))
+                // find target
+                return true;
+        // not find target
+        return false;
+    }
+
+    double frogPosition(int n, vector<vector<int>> &edges, int t, int target) {
+        graph = vector<vector<int>>(n + 1);
+        graph[1] = {0};
+        for (auto &e: edges) {
+            int x = e[0], y = e[1];
+            graph[x].push_back(y);
+            graph[y].push_back(x);
+        }
+        this->t = t;
+        this->target = target;
+
+        DFS(1, 0, t, 1);
+        return ans;
+    }
+};
+
 // WA
 class Solution {
 public:
@@ -49,43 +88,5 @@ public:
         int pos = DFS(1, t, visited);
 
         return (double)match / pos;
-    }
-};
-
-// endlesscheng
-class Solution {
-public:
-    int t;
-    int target;
-    double ans = 0;
-    vector<vector<int>> graph;
-    int DFS(int x, int parent, int left_time, long long prod) {
-        if (x == target && (left_time == 0 || graph[x].size() == 1)) {
-            ans = 1.0 / prod;
-            return true;
-        }
-        if (x == target || left_time == 0) 
-            return false;
-        for (int y: graph[x])  
-            if (y != parent && DFS(y, x, left_time - 1, prod * (graph[x].size() - 1)))
-                // find target
-                return true;
-        // not find target
-        return false;
-    }
-
-    double frogPosition(int n, vector<vector<int>> &edges, int t, int target) {
-        graph = vector<vector<int>>(n + 1);
-        graph[1] = {0};
-        for (auto &e: edges) {
-            int x = e[0], y = e[1];
-            graph[x].push_back(y);
-            graph[y].push_back(x);
-        }
-        this->t = t;
-        this->target = target;
-
-        DFS(1, 0, t, 1);
-        return ans;
     }
 };
