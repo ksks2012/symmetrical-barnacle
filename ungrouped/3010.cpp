102f78e058d5e7026adcf9d6a7af16edbcd2fcec
ksks2012 <st890751999@gmail.com>
2024-01-21 00:48:22 +0800

    feat(3010): Divide an Array Into Subarrays With Minimum Cost I

---
+++ ungrouped/3010.cpp
@@ -0,0 +1,10 @@
+#include "include.h"
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        sort(nums.begin() + 1, nums.end());
+
+        return nums[0] + nums[1] + nums[2];
+    }
+};
