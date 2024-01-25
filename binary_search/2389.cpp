6b81c2d2b84854c2b606eb0b9fb9e17765e4f3db
ksks2012 <st890751999@gmail.com>
2024-01-26 00:42:34 +0800

    feat(2389): Longest Subsequence With Limited Sum

---
+++ binary_search/2389.cpp
@@ -0,0 +1,65 @@
+#include "include.h"
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int res = -1;
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            cout << nums[mid] << endl;
+            if(nums[mid] == target) {
+                return mid + 1;
+            }
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<int> ps(n, 0);
+        int tmp = 0;
+        for(int i = 0; i < n; i++) {
+            tmp += nums[i];
+            ps[i] = tmp;
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++) {
+            ans.push_back(search(ps, queries[i]));
+        }
+
+        return ans;      
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<int> ps(n, 0);
+        int tmp = 0;
+        for(int i = 0; i < n; i++) {
+            tmp += nums[i];
+            ps[i] = tmp;
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++) {
+            ans.push_back(upper_bound(ps.begin(), ps.end(), queries[i]) - ps.begin());
+        }
+
+        return ans;      
+    }
+};
