4f86c4c383446a7bdd8665fd25397ef80fed83a2
ksks2012 <st890751999@gmail.com>
2024-01-26 16:30:46 +0800

    refactor(33): Format

--- binary_search/33.cpp
+++ binary_search/33.cpp
@@ -37,7 +37,8 @@ class Solution {
         int left = -1, right = nums.size() - 1; // (-1, n-1)
         while (left + 1 < right) { // not null
             int mid = left + (right - left) / 2;
-            if (nums[mid] < nums.back()) right = mid; // blue
+            if (nums[mid] < nums.back()) 
+                right = mid; // blue
             else left = mid; // red
         }
         return right;
@@ -59,8 +60,10 @@ class Solution {
 public:
     int search(vector<int> &nums, int target) {
         int i = findMin(nums), left, right;
-        if (target > nums.back()) left = -1, right = i; // left part
-        else left = i - 1, right = nums.size(); // right part
+        if (target > nums.back()) 
+            left = -1, right = i; // left part
+        else 
+            left = i - 1, right = nums.size(); // right part
         return lower_bound(nums, left, right, target);
     }
 };
