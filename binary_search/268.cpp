86a9e835f65459b5cde55593cc808238559bcacd
ksks2012 <st890751999@gmail.com>
2024-01-27 15:11:02 +0800

    refactor(268): Format

--- binary_search/268.cpp
+++ binary_search/268.cpp
@@ -6,9 +6,9 @@ public:
         sort(nums.begin(), nums.end());
         int left = 0, right = nums.size() - 1;
         
-        while(left <= right){
+        while(left <= right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] == mid){
+            if(nums[mid] == mid) {
                 left = mid + 1;
             } else{
                 right = mid - 1;
