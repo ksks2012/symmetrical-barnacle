0b8d1552325c2a589d6f91308bd61f7eddf593bc
ksks2012 <st890751999@gmail.com>
2024-01-29 21:26:51 +0800

    feat(35): Search Insert Position

---
+++ binary_search/35.cpp
@@ -0,0 +1,75 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(target == nums[mid]) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left] < target ? left + 1: left;
+    }
+};
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        if (target > nums[n - 1]) {
+            return n;
+        }
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(target == nums[mid]) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+};
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n;
+
+        if (target > nums[n - 1]) {
+            return n;
+        }
+
+        while(left < right) {
+            int mid = left + right >> 1;
+            if(target == nums[mid]) {
+                return mid;
+            } else if(nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
