442e80bde65908698ff0f8d3eb9ba52dd5fc5bb6
ksks2012 <st890751999@gmail.com>
2024-01-31 18:24:21 +0800

    refactor(1237): Format

--- binary_search/1237.cpp
+++ binary_search/1237.cpp
@@ -29,9 +29,9 @@ public:
         int y = 1000;
 
         for(int x = 1; x <= 1000; x++) {
-            while(y > 1 && customfunction.f(x,y) > z)
+            while(y > 1 && customfunction.f(x, y) > z)
                 y--;
-            if(customfunction.f(x,y) == z)
+            if(customfunction.f(x, y) == z)
                 ans.push_back({x, y});
         }
 
