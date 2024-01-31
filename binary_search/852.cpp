34bc52b8f56afe3480e49e49a71c80efc4c2959d
ksks2012 <st890751999@gmail.com>
2024-01-31 21:57:26 +0800

    feat(852): Peak Index in a Mountain Array

---
+++ binary_search/852.cpp
@@ -0,0 +1,20 @@
+#include "include.h"
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }   
+        }
+        return left;
+    }
+};
