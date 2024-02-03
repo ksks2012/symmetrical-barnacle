5e850500fd8c2a6faf4a939e5a673575045531c4
ksks2012 <st890751999@gmail.com>
2024-02-04 00:26:38 +0800

    feat(2358): Update

--- binary_search/2358.cpp
+++ binary_search/2358.cpp
@@ -13,6 +13,21 @@ public:
 
 class Solution {
 public:
+    int maximumGroups(vector<int> &grades) {
+        int left = 1, right = 446, n = grades.size();
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (mid * (mid + 1) / 2 <= n)
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+};
+
+class Solution {
+public:
     int maximumGroups(vector<int> &g) {
         int left = 1;
         int right = 1000;
@@ -32,5 +47,28 @@ public:
             }
         }
         return mid;
+    }
+};
+
+// Greedy
+class Solution {
+public:
+    int maximumGroups(vector<int> &g) {
+        sort(g.begin(), g.end());
+        int pre_sum = 0;
+        int k = 0;
+        int n = g.size();
+        for(int i = 0; i < n; i++) {
+            int tmp = i, sum = 0;
+            for(; i < n && sum <= pre_sum || i <= tmp + k; i++) {
+                sum += g[i];
+            }
+            if(sum <= pre_sum) {
+                break;
+            }
+            pre_sum = sum;
+            k = i - tmp;
+        }
+        return 
     }
 };
