3415d8e8c902577049f24e7ef88b0be3f0993525
ksks2012 <st890751999@gmail.com>
2024-02-06 23:57:46 +0800

    feat(2485): Update solution with binary search

--- prefix_sum/2485.cpp
+++ prefix_sum/2485.cpp
@@ -19,3 +19,32 @@ public:
         return -1;
     }
 };
+
+// With binary search
+class Solution {
+public:
+    int pivotInteger(int n) {
+        vector<int> sum(n + 2, 0);
+        int tmp = 0;
+        for(int i = 0; i <= n; i++) {
+            tmp += i;
+            sum[i + 1] = tmp;
+        }
+
+        int left = 0;
+        int right = n;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if((sum[mid + 1] - sum[0]) == (sum[n + 1] - sum[mid])) {
+                return mid;
+            } else if((sum[mid + 1] - sum[0]) < (sum[n + 1] - sum[mid])) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
