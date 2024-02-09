00ccf9b4e7c988a941f860ebda4fd24e5da6c333
ksks2012 <st890751999@gmail.com>
2024-02-09 10:30:50 +0800

    feat(2411): Update more versatile Solution

--- two_pointer/2411.cpp
+++ two_pointer/2411.cpp
@@ -17,6 +17,30 @@ public:
     }
 };
 
+// endlesscheng
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int> &nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        vector<pair<int, int>> ors; // (OR by bits, Rightmost and smallest value)
+        for (int i = n - 1; i >= 0; --i) {
+            ors.emplace_back(0, i);
+            ors[0].first |= nums[i];
+            int k = 0;
+            for (int j = 1; j < ors.size(); ++j) {
+                ors[j].first |= nums[i];
+                if (ors[k].first == ors[j].first)
+                    ors[k].second = ors[j].second; // merge same value, obtain smallest
+                else ors[++k] = ors[j];
+            }
+            ors.resize(k + 1);
+            ans[i] = ors[0].second - i + 1;
+        }
+        return ans;
+    }
+};
+
 // WA
 class Solution {
 public:
