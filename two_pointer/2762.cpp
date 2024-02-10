921846f61f35bb95ceb149a5c865879695cfa2d6
ksks2012 <st890751999@gmail.com>
2024-02-10 09:38:14 +0800

    feat(2762): Update Solution with multiset

--- two_pointer/2762.cpp
+++ two_pointer/2762.cpp
@@ -36,3 +36,20 @@ public:
         return ans;        
     }
 };
+
+// endlesscheng
+class Solution {
+public:
+    long long continuousSubarrays(vector<int> &nums) {
+        long long ans = 0;
+        multiset<int> s;
+        int left = 0, n = nums.size();
+        for (int right = 0; right < n; right++) {
+            s.insert(nums[right]);
+            while (*s.rbegin() - *s.begin() > 2)
+                s.erase(s.find(nums[left++]));
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+};
