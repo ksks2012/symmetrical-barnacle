a4362ac2c74f95437a666435e4ebd3cb9e86670b
ksks2012 <st890751999@gmail.com>
2024-02-15 22:00:33 +0800

    refactor(2653): Update variable name

--- two_pointer/2653.cpp
+++ two_pointer/2653.cpp
@@ -3,25 +3,25 @@
 class Solution {
 public:
     vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-        int n = nums.size(), j = 0;
+        int n = nums.size(), left = 0;
         
         multiset<int> ms;
         vector<int> res;
         
-        for(int i = 0; i < n; i++) {
-            if(nums[i] < 0) 
-                ms.insert(nums[i]);
+        for(int right = 0; right < n; right++) {
+            if(nums[right] < 0) 
+                ms.insert(nums[right]);
 
-            if(i - j + 1 == k) {
+            if(right - left + 1 == k) {
                 if(ms.size() < x)
                     res.push_back(0);
                 else 
                     res.push_back(*next(ms.begin(), x - 1));
                 
-                if(nums[j] < 0) 
-                    ms.erase(ms.find(nums[j]));
+                if(nums[left] < 0) 
+                    ms.erase(ms.find(nums[left]));
                 
-                j++;
+                left++;
             }
         }
         
