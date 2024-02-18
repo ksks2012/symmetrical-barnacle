e545b89a63d183bcb82e149778bc5ece6cdbae9d
ksks2012 <st890751999@gmail.com>
2024-02-18 16:28:46 +0800

    feat(3038): Update

--- ungrouped/3038.cpp
+++ ungrouped/3038.cpp
@@ -29,3 +29,19 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums) {
+        int n = nums.size();
+        int target = nums[0] + nums[1];
+        int ans = 0;
+        for(int i = 0;i < n - 1; i += 2){
+            if(target == (nums[i] + nums[i + 1])) 
+                ans++;
+            else 
+                break;
+        }
+        return ans;
+    }
+};
