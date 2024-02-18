56c6ebce1acb6de6546f2ad0f6a272eb8209a1b5
ksks2012 <st890751999@gmail.com>
2024-02-18 17:00:10 +0800

    feat(2824): Update solution with two pointer

--- two_pointer/2824.cpp
+++ two_pointer/2824.cpp
@@ -14,3 +14,21 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                count += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+};
