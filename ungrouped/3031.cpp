06ae58f9349c416fb3c99138d8f3363890ddc013
ksks2012 <st890751999@gmail.com>
2024-02-21 05:49:18 +0800

    feat(3031): Update

--- ungrouped/3031.cpp
+++ ungrouped/3031.cpp
@@ -17,10 +17,16 @@ public:
         int n = s.size(), left = 0, right = 0;
         vector<int> z(n);
         for(int i = 1; i < n; i++) {
+            // Had discovered, start from previous result
             if(i < right)
                 z[i] = min(right - i, z[i - left]);
-            while(i + z[i] < n && s[z[i]] == s[i + z[i]])
+            // Compare string util different
+            while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
                 z[i]++;
+            }
+            // Update left and right
+            // left: current start
+            // right: current end point
             if(i + z[i] > right) {
                 left = i;
                 right = i + z[i];
