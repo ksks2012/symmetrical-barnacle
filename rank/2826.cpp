e6a329829c29cef6567f8023e94bdb1e2cd1d32d
ksks2012 <st890751999@gmail.com>
2024-03-15 09:05:43 +0800

    doc(2826): Update

--- rank/2826.cpp
+++ rank/2826.cpp
@@ -8,10 +8,14 @@ public:
     int minimumOperations(vector<int> &nums) {
         vector<int> tmp;
         for (int x : nums) {
+            // 1, 6, 7
+            // 1, 2, 7
+            // Non-strict incremental
             auto it = upper_bound(tmp.begin(), tmp.end(), x);
             if (it == tmp.end()) 
                 tmp.push_back(x);
-            else *it = x;
+            else 
+                *it = x;
         }
         return nums.size() - tmp.size();
     }
