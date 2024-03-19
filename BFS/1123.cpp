1b44624fae8b3e2e07c7be942da43e5dea870c6e
ksks2012 <st890751999@gmail.com>
2024-03-20 06:40:37 +0800

    feat(1123): Lowest Common Ancestor of Deepest Leaves

---
+++ BFS/1123.cpp
@@ -0,0 +1,56 @@
+#include "include.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode*, int> helper(TreeNode* root) {
+        if (!root) 
+            return {NULL, 0};
+        auto left = helper(root->left);
+        auto right = helper(root->right);
+        if (left.second > right.second)
+            return {left.first, left.second + 1};
+        if (left.second < right.second)
+            return {right.first, right.second + 1};
+        return {root, left.second + 1};
+
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return helper(root).first;
+    }
+};
+
+class Solution {
+private:
+    TreeNode* lca;
+    int deepest = 0;
+public:
+    int helper(TreeNode* node, int depth) {
+        deepest = max(deepest, depth);
+        if (!node) {
+            return depth;
+        }
+        int left = helper(node->left, depth + 1);
+        int right = helper(node->right, depth + 1);
+        if (left == deepest && right == deepest) {
+            lca = node;
+        }
+        return max(left, right);
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        helper(root, 0);
+        return lca;
+    }
+};
