803bd82120fcc4cf219aaf71ccd444971611cf42
ksks2012 <st890751999@gmail.com>
2024-03-24 22:47:39 +0800

    feat(2438): Add brute force Solution

--- rank/2438.cpp
+++ rank/2438.cpp
@@ -11,14 +11,37 @@ public:
         return y % 2 ? (p * x) % m : p;
     }    
 
-    vector<int> productQueries(int n, vector<vector<int>>& qs) {
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
         vector<int> pws{1}, res;
         for (int b = 0; b < 30; ++b)
             if (n & (1 << b))
                 pws.push_back((1LL << b) * pws.back() % MOD);
-        transform(begin(qs), end(qs), back_inserter(res), [&](const auto &q) { 
+        transform(begin(queries), end(queries), back_inserter(res), [&](const auto &q) { 
             return (long long)pws[q[1] + 1] * modPow(pws[q[0]], MOD - 2, MOD) % MOD; 
         });
+        return res;
+    }
+};
+
+// brute force
+# define MOD 1000000007
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> a;
+        while (n > 0) {
+            int lowbit = n & -n;
+            a.push_back(lowbit);
+            n ^= lowbit;
+        }
+        vector<int> res(queries.size());
+        for (int i = 0; i < queries.size(); ++i) {
+            int mul = 1;
+            for (int j = queries[i][0]; j <= queries[i][1]; ++j) {
+                mul = (1LL * mul * a[j]) % MOD;
+            }
+            res[i] = mul;
+        }
         return res;
     }
 };
