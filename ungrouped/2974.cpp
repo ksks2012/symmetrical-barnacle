f295ebd6673e7b9fbd861c88716e84cb0f56bc90
ksks2012 <st890751999@gmail.com>
2024-03-25 17:31:24 +0800

    feat(2974): Add heap Solution

--- ungrouped/2974.cpp
+++ ungrouped/2974.cpp
@@ -9,16 +9,43 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size(); i+=2) {
+        for(int i = 0; i < nums.size(); i += 2) {
             a.push(nums[i]);
             b.push(nums[i + 1]);
         }
 
-        for(int i = 0; i < nums.size(); i+= 2) {
+        for(int i = 0; i < nums.size(); i += 2) {
             ans.push_back(b.front());
             b.pop();
             ans.push_back(a.front());
             a.pop();
+        }
+
+        return ans;
+    }
+};
+
+// Heap
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        vector<int> ans(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            pq.push(nums[i]);
+        }
+
+        int i = 0;
+        while(pq.size() > 0) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            ans[i++] = b;
+            ans[i++] = a;
         }
 
         return ans;
