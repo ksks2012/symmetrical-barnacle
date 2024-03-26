018c46796d340424b8383a2c421a3f22be16c5cf
ksks2012 <st890751999@gmail.com>
2024-03-27 07:52:34 +0800

    feat(3092):
    - Rewrite pq version
    - Add Solution of multiset

--- ungrouped/3092.cpp
+++ ungrouped/3092.cpp
@@ -1,26 +1,46 @@
 #include "include.h"
 
+// endlesscheng
 class Solution {
 public:
     vector<long long> mostFrequentIDs(vector<int> &nums, vector<int> &freq) {
-        priority_queue<pair<long long, int>> pq;
+        int n = nums.size();
+        vector<long long> res(n);
         unordered_map<int, long long> m;
-        vector<long long> res;
-        for (int i = 0; i < nums.size(); i++) {
-            m[nums[i]] += freq[i];
-            pq.push({m[nums[i]], nums[i]});
-            while (!pq.empty()) {
-                pair<long long, int> p = pq.top();
-                // Update
-                if (m[p.second] != p.first) {
-                    pq.pop();
-                    if (m[p.second])
-                        pq.push({m[p.second], p.second});
-                } else {
-                    break;
-                }
+        priority_queue<pair<long long, int>> pq;
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            m[x] += freq[i];
+            // {freq, nums}
+            pq.emplace(m[x], x);
+            // top of heap has changed
+            while (pq.top().first != m[pq.top().second]) {
+                // delete
+                pq.pop();
             }
-            res.push_back(pq.top().first);
+            res[i] = pq.top().first;
+        }
+        return res;
+    }
+};
+
+// hash table + multiset
+class Solution {
+public:
+    vector<long long> mostFrequentIDs(vector<int> &nums, vector<int> &freq) {
+        unordered_map<int, long long> m;
+        multiset<long long> s;
+        int n = nums.size();
+        vector<long long> res(n);
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            auto it = s.find(m[x]);
+            if (it != s.end()) {
+                s.erase(it);
+            }
+            m[x] += freq[i];
+            s.insert(m[x]);
+            res[i] = *s.rbegin();
         }
         return res;
     }
