a1e8ebdd897e9d6cc41fbe65cae86d69c688784c
ksks2012 <st890751999@gmail.com>
2024-03-28 21:41:29 +0800

    feat(2365): Task Scheduler II

---
+++ rank/2365.cpp
@@ -0,0 +1,20 @@
+#include "include.h"
+
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        long long res = 0;
+        unordered_map<int, int> last_task;
+        for(int i = 0; i < tasks.size(); i++) {
+            if(last_task.find(tasks[i]) != last_task.end()) {
+                int diff = res - last_task[tasks[i]] - 1;
+                res += space > diff ? space - diff: 1;
+            } else {
+                res += 1;
+            }
+            last_task[tasks[i]] = res;
+        }
+
+        return res;
+    }
+};
