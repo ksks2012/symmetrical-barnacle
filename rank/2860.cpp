0d2b54dec56c60f59dff36d082390f167ffad674
ksks2012 <st890751999@gmail.com>
2024-04-01 20:48:00 +0800

    doc(2860): Update

--- rank/2860.cpp
+++ rank/2860.cpp
@@ -7,15 +7,18 @@ public:
         int n = nums.size();
         int res = 0;
 
+        // Not Selecting
         if(nums[0] != 0) {
             res = 1;
         }
 
         for(int i = 0; i < n; i++) {
             if(nums[i] < i + 1) {
+                // Considering from (i+1) to n Students is not Selected.
                 if(i + 1 < n && i + 1 < nums[i + 1]) {
                     res++;
                 } else if(i + 1 == n) {
+                    // Last Student
                     res++;
                 }
             }
