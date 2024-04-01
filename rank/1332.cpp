bc19eb588f0b9ee0a669d4c5acefe2b4ca9ec901
ksks2012 <st890751999@gmail.com>
2024-04-01 08:28:13 +0800

    feat(1332): Update

--- rank/1332.cpp
+++ rank/1332.cpp
@@ -1,8 +1,24 @@
 #include "include.h"
 
+// lee
 class Solution {
 public:
     int removePalindromeSub(string s) {
         return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
+    }
+};
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int left = 0, right = s.size() - 1;
+        while(left < right) {
+            if(s[left] != s[right]) 
+                return 2;
+            left++;
+            right--;
+        }
+        
+        return 1;
     }
 };
