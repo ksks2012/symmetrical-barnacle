88f5d8ec3bef11d9b4a9bb0cd5958a76cef1ab18
ksks2012 <st890751999@gmail.com>
2024-04-01 14:57:24 +0800

    feat(1962): Remove Stones to Minimize the Total

---
+++ heap/1962.cpp
@@ -0,0 +1,44 @@
+#include "include.h"
+
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+
+        for(auto p: piles) {
+            pq.push(p);
+        }
+
+        for(int i = 0; i < k; i++) {
+            int tmp = pq.top();
+            pq.pop();
+            tmp -= floor((double)tmp / 2);
+            pq.push(tmp);
+        }
+        
+        int res = 0;
+        while(!pq.empty()) {
+            res += pq.top();
+            pq.pop();
+        }
+
+        return res;
+    }
+};
+
+// lee
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq(piles.begin(), piles.end());
+        // prefix sum
+        int res = accumulate(piles.begin(), piles.end(), 0);
+        while (k--) {
+            int tmp = pq.top();
+            pq.pop();
+            pq.push(tmp - tmp / 2);
+            res -= tmp / 2;
+        }
+        return res;
+    }
+};
