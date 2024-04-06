7b333d97387c95bad94456000f2d1a75209ff295
ksks2012 <st890751999@gmail.com>
2024-04-06 09:47:59 +0800

    feat(2187): Minimum Time to Complete Trips

---
+++ rank/2187.cpp
@@ -0,0 +1,28 @@
+#include "../include.h"
+
+class Solution {
+public:
+    long long helper(vector<int>& time, long long give) {
+        long long total = 0;
+        for(auto t: time) {
+            long long val = t;
+            total += (give / val);
+        }
+        return total;
+    }
+
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        long long low = 1;
+        long long high = INT_MAX;
+
+        while(low < high) {
+            long long mid = low + (high - low) / 2;
+            if(helper(time, mid) >= totalTrips) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
