bc3845b0ff5002d8f1ffe6155b40d74950990c3a
ksks2012 <st890751999@gmail.com>
2024-04-09 21:11:05 +0800

    feat(2423): Update Solution of brute force

--- rank/2423.cpp
+++ rank/2423.cpp
@@ -17,3 +17,27 @@ public:
                count.back() == count[count.size() - 2] + 1 && equal(count.begin() + 1, count.end() - 1, count.begin());
     }
 };
+
+// brute force
+class Solution {
+    bool is_same(unordered_map<char, int> &cnt) {
+        int c0 = cnt.begin()->second;
+        for (auto &[_, c]: cnt)
+            if (c != c0)
+                return false;
+        return true;
+    }
+public:
+    bool equalFrequency(string word) {
+        int n = word.length();
+        for (int i = 0; i < n; ++i) { 
+            unordered_map<char, int> count;
+            for (int j = 0; j < n; ++j)
+                if (j != i)
+                    ++count[word[j]];
+            if (is_same(count)) // same number of count
+                return true;
+        }
+        return false;
+    }
+};
