17cef51402b23c65b31204899f4d6b8ba0251fd8
ksks2012 <st890751999@gmail.com>
2024-04-10 14:37:32 +0800

    feat(3080): Update Solution

--- ungrouped/3080.cpp
+++ ungrouped/3080.cpp
@@ -76,3 +76,35 @@ public:
         return res;
     }
 };
+
+// endlesscheng
+class Solution {
+public:
+    vector<long long> unmarkedSumArray(vector<int> &nums, vector<vector<int>> &queries) {
+        int n = nums.size();
+        long long s = accumulate(nums.begin(), nums.end(), 0LL);
+        vector<int> ids(n);
+        iota(ids.begin(), ids.end(), 0);
+        ranges::stable_sort(ids, [&](int i, int j) { 
+            return nums[i] < nums[j]; 
+        });
+
+        vector<long long> ans;
+        int j = 0;
+        for (auto &q : queries) {
+            int i = q[0], k = q[1];
+            s -= nums[i];
+            nums[i] = 0; // mark
+            for (; j < n && k; j++) {
+                i = ids[j];
+                if (nums[i] > 0) { // do not marked
+                    s -= nums[i];
+                    nums[i] = 0;
+                    k--;
+                }
+            }
+            ans.push_back(s);
+        }
+        return ans;
+    }
+};
