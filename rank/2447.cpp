61ffdf873d687f0fb50106c160161f5accc384ca
ksks2012 <st890751999@gmail.com>
2024-04-12 09:39:58 +0800

    feat(2447): Update

--- rank/2447.cpp
+++ rank/2447.cpp
@@ -27,12 +27,38 @@ public:
             unordered_map<int, int> gcds1;
             if (nums[i] % k == 0) {
                 ++gcds[nums[i]];
+                // clear repeating GCD
                 for (auto [prev_gcd, cnt] : gcds)
                     gcds1[gcd(prev_gcd, nums[i])] += cnt;
             }
             res += gcds1[k];
             swap(gcds, gcds1);
         }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k) {
+        int res = 0;
+        vector<pair<int,int>> tmp({{-1, 0}});
+
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = 1; j < tmp.size(); ++j)
+                tmp[j].second = gcd(tmp[j].second, nums[i]);
+            tmp.push_back({i, nums[i]});
+
+            for(int j = (int)tmp.size() - 2; j >= 1; --j)
+                if(tmp[j].second == tmp[j+1].second)
+                    tmp[j].first = tmp[j+1].first;
+            tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
+
+            for(int j = 1; j < tmp.size(); ++j)
+                if(tmp[j].second == k)
+                    res += tmp[j].first - tmp[j-1].first;
+        }
+
         return res;
     }
 };
