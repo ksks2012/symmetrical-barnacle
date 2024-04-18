744bc81225c2ad46bbf52d2e38d314fe1f32ea94
ksks2012 <st890751999@gmail.com>
2024-04-18 13:29:12 +0800

    feat(1992): Update

--- rank/1992.cpp
+++ rank/1992.cpp
@@ -1,5 +1,6 @@
 #include "include.h"
 
+// Flood-Fill
 class Solution {
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
@@ -18,3 +19,23 @@ public:
         return res;
     }
 };
+
+// Mark Left Side
+vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    int m = land.size(), n = land[0].size();
+    vector<vector<int>> res;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j) {
+            if (land[i][j] == 1) {
+                int x = i, y = j;
+                while (y < n && land[i][y])
+                    ++y;                
+                for (; x < m && land[x][j]; ++x)
+                    land[x][j] = y - j + 1;
+                res.push_back({i, j, x - 1, y - 1});
+            }
+            if (land[i][j] > 1)
+                j += land[i][j] - 1;
+        }
+    return res;
+}  
