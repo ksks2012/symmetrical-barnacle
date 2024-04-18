f57a018f76abd89c0b71b2fd0e17be876f36c948
ksks2012 <st890751999@gmail.com>
2024-04-18 17:27:16 +0800

    feat(2349): Design a Number Container System

---
+++ design/2349.cpp
@@ -0,0 +1,33 @@
+#include "include.h"
+
+class NumberContainers {
+public:
+    unordered_map<int, int> idx_val;
+    unordered_map<int, set<int>> num_idx;
+
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        if(idx_val.find(index) != idx_val.end() && idx_val[index] != number) {
+            num_idx[idx_val[index]].erase(index);
+        }
+        idx_val[index] = number;
+        num_idx[number].insert(index);
+    }
+    
+    int find(int number) {
+        if(num_idx.find(number) != num_idx.end() && num_idx[number].size() > 0) {
+            return *num_idx[number].begin(); 
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
