9c54d5b4e1e24bc0690c80f5110d5d962a0f2b3c
ksks2012 <st890751999@gmail.com>
2024-04-19 16:12:29 +0800

    doc(2536): Update

--- prefix_sum/2536.cpp
+++ prefix_sum/2536.cpp
@@ -31,16 +31,20 @@ public:
         for (auto q : queries) {
                 int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
                 ++res[r1][c1];
+                // bottom left
                 if (r2 + 1 < n)
                     --res[r2 + 1][c1];
+                // top right
                 if (c2 + 1 < n)
                     --res[r1][c2 + 1];
+                // bottom right
                 if (max(r2, c2) + 1 < n)
                     ++res[r2 + 1][c2 + 1];
         }
-        // prefix sum
+        // prefix sum: Left to right
         for (int i = 0; i < n; ++i)
             partial_sum(begin(res[i]), end(res[i]), begin(res[i]));  
+        // prefix sum: Top to bottom
         for (int i = 1; i < n; ++i)
             for (int j = 0; j < n; ++j)
                 res[i][j] += res[i - 1][j];         
