f30078318cef24223aaf1b7a2ebf71d2eaa9d1ea
ksks2012 <st890751999@gmail.com>
2024-04-20 18:38:45 +0800

    feat(42): Add Solution with stack

--- two_pointer/42.cpp
+++ two_pointer/42.cpp
@@ -57,3 +57,26 @@ public:
         return res;
     }
 };
+
+// endlesscheng
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        stack<int> st;
+        for (int i = 0; i < height.size(); i++) {
+            while (!st.empty() && height[i] >= height[st.top()]) {
+                int bottom_h = height[st.top()];
+                st.pop();
+                if (st.empty()) {
+                    break;
+                }
+                int left = st.top();
+                int dh = min(height[left], height[i]) - bottom_h; // height of area
+                ans += dh * (i - left - 1);
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
