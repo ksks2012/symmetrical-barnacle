93d6214b3e3870876db4e1f41df413a9978137fd
ksks2012 <st890751999@gmail.com>
2024-04-22 10:49:32 +0800

    feat(2365): Update

--- rank/2365.cpp
+++ rank/2365.cpp
@@ -18,3 +18,20 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        long long res = 0;
+        unordered_map<int, long long> last_task;
+        for(int i = 0; i < tasks.size(); i++) {
+            if(last_task.count(tasks[i])) {
+                last_task[tasks[i]] = res = max(res, last_task[tasks[i]] + space) + 1;
+            } else {
+                last_task[tasks[i]] = ++res;
+            }
+        }
+
+        return res;
+    }
+};
