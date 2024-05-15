3685d639c850a9c7ad7426d516ab2af29622843b
ksks2012 <st890751999@gmail.com>
2024-05-15 15:17:55 +0800

    feat(1268): Add Solution using trie

--- binary_search/1268.cpp
+++ binary_search/1268.cpp
@@ -27,3 +27,49 @@ public:
         return ans;
     }
 };
+
+// Using Trie
+struct Trie {
+    unordered_map<char, Trie*> children;
+    // Lexographically sorted suggestion till each char 
+    vector<string> suggestions;
+};
+
+class Solution {
+public:
+
+    void insert_trie(Trie* root, string word) {
+        for(const char& c: word) {
+            if(root->children.find(c) == root->children.end())
+                root->children[c] = new Trie();
+            root = root->children[c];
+            
+            // Limit the suggestions to 3
+            if(root->suggestions.size() < 3)
+                root->suggestions.emplace_back(word);
+        }
+    }
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        vector<vector<string>> result(searchWord.size());
+        sort(begin(products), end(products));
+        
+        // Create the Trie, since the words are sorted
+        // so the suggestions are also lexographically sorted
+        Trie *root = new Trie();
+        for(const string &word: products)
+            insert_trie(root, word);
+        
+        string prefix;
+        for(int i = 0; i < searchWord.size(); i++) {
+            auto it = root->children.find(searchWord[i]);
+            if(it != root->children.end()) {
+                root = it->second;
+            }
+            else
+                break;
+            // add suggestions if any for the current char
+            result[i] = root->suggestions;
+        }
+        return result;
+    }
+};
