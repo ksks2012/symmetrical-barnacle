ed76f2fef3fd436cd0ae3a8c4cbd88d011f5eedc
ksks2012 <st890751999@gmail.com>
2024-05-19 18:09:00 +0800

    doc(2187): Update

--- rank/2187.cpp
+++ rank/2187.cpp
@@ -2,6 +2,7 @@
 
 class Solution {
 public:
+    // Helper function to calculate the total number of trips that can be completed within a given time limit
     long long helper(vector<int>& time, long long give) {
         long long total = 0;
         for(auto t: time) {
@@ -11,6 +12,7 @@ public:
         return total;
     }
 
+    // Function to find the minimum time required to complete a given number of trips
     long long minimumTime(vector<int>& time, int totalTrips) {
         long long low = 1;
         long long high = 1e14;
