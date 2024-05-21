ad2c7741d7093b17226a22bef95498eaf893453d
ksks2012 <st890751999@gmail.com>
2024-05-21 14:11:18 +0800

    doc(2226): Update

--- rank/2226.cpp
+++ rank/2226.cpp
@@ -5,10 +5,13 @@ public:
     int maximumCandies(vector<int>& candies, long long k) {
         int left = 0, right = 10000000;
         while(left < right) {
-            long long mid = (left + right + 1) / 2, cnt = 0;
-            for (int i = 0; i < candies.size() && cnt < k; ++i)
-                cnt += candies[i] / mid;
-            if (cnt >= k)
+            // mid -> number of candies to be distributed
+            long long mid = (left + right + 1) / 2, count = 0;
+            // calculate the number of candies that can be distributed
+            for (int i = 0; i < candies.size() && count < k; ++i)
+                count += candies[i] / mid;
+            // update the left and right pointers
+            if (count >= k)
                 left = mid;
             else
                 right = mid - 1;
