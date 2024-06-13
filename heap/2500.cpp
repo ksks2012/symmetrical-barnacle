75a8aa746374da6d8b166e9d516fa7223883d5eb
ksks2012 <st890751999@gmail.com>
2024-06-13 20:41:23 +0800

    feat(2500): Add Solution with sorting

--- heap/2500.cpp
+++ heap/2500.cpp
@@ -5,10 +5,8 @@ public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
         int res = 0;
 
-
         int m = grid.size();
         int n = grid[0].size();
-
 
         priority_queue<int> pq[m];
         for(int i = 0; i < m; i++) {
@@ -27,5 +25,40 @@ public:
         }
 
         return res;
+    }
+};
+
+
+class Solution {
+public:
+    static bool cmp(int a , int b){
+        return a > b;
+    }
+
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int ans = 0;
+
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> mat = grid;
+        
+        
+        for(int i = 0; i < m; i++){
+            sort(mat[i].begin(), mat[i].end(), cmp);
+        }
+        
+        for(int j = 0; j < n; j++){
+            vector<int> tmp(mat.size());
+            for(int i = 0; i < m; i++){
+               
+                tmp.push_back(mat[i][j]);
+            }
+           
+            sort(tmp.begin(), tmp.end(), cmp);
+            
+            ans = ans + tmp[0];
+        }
+        return ans;
     }
 };
