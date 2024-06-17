66fdbd7ffef0bfa7b74c4bf9ebf7e8375b86251f
ksks2012 <st890751999@gmail.com>
2024-06-17 22:10:23 +0800

    feat(692): Top K Frequent Words

---
+++ heap/692.cpp
@@ -0,0 +1,28 @@
+#include "include.h"
+
+class Solution {
+    struct compare{
+        bool operator()(pair<int,string>& a,pair<int,string>& b) {
+            if(a.first==b.first)
+                return a.second > b.second; //sort assending 
+            return a.first<b.first; //sorts descending 
+        }
+    };
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string, int> m;
+        for(auto x : words){
+            m[x] += 1;
+        }
+        priority_queue<pair<int, string>, vector<pair<int, string>>, compare> pq;
+        for(auto i : m) {
+            pq.push({i.second, i.first});
+        }
+        vector<string> res;
+        while(k--) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
