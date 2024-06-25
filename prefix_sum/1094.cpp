12fffc37a217e88d9dd2dc8e3cf9943ffe8a0088
ksks2012 <st890751999@gmail.com>
2024-06-25 23:11:21 +0800

    feat(1094): Add Solution with Heap

--- prefix_sum/1094.cpp
+++ prefix_sum/1094.cpp
@@ -17,3 +17,41 @@ public:
         return capacity >= 0;
     }
 };
+
+class Solution {
+public:
+    
+    static bool comp(vector<int> left, vector<int> right) {
+        if(left[1] < right[1] || (left[1] == right[1] && left[2] < right[2]))
+            return true;
+        return false;
+    }
+        
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int n = trips.size();
+		
+        sort(trips.begin(), trips.end(),comp);
+        
+        int curr = 0;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; // destinaton , numpassengers
+        
+        for(int i = 0; i < n; i++) {
+			// step 1 
+            while(!pq.empty() && pq.top().first <= trips[i][1]) {
+                curr -= pq.top().second;
+                pq.pop();
+            }
+            
+			// step 2
+            pq.push({trips[i][2], trips[i][0]});
+            curr += trips[i][0];
+			
+			//step 3
+            if(curr > capacity)
+                return false;
+        }
+		
+        return true;  
+    }
+};
