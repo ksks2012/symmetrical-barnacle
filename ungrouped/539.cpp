b6aee62b0c3844f19f5575265ac4b68fb9089ce2
ksks2012 <st890751999@gmail.com>
2024-10-01 19:11:22 +0800

    fix(539)

--- ungrouped/539.cpp
+++ ungrouped/539.cpp
@@ -3,6 +3,24 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        
+        vector<int> tmp;
+
+        int res = INT_MAX;
+
+        for (string time : timePoints) {
+            int hour = stoi(time.substr(0, 2));
+            int minute = stoi(time.substr(3, 2));
+            tmp.push_back(hour * 60 + minute);
+        }
+
+        sort(tmp.begin(), tmp.end());
+
+        for (int i = 1; i < tmp.size(); i++) {
+            res = min(res, tmp[i] - tmp[i - 1]);
+        }
+
+        res = min(res, 1440 - tmp.back() + tmp[0]);
+
+        return res;
     }
 };
