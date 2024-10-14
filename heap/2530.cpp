202c9ca24c63d680eca729bcf74b58c995cfc05a
ksks2012 <st890751999@gmail.com>
2024-10-14 15:19:23 +0800

    feat(2530): Update

--- heap/2530.cpp
+++ heap/2530.cpp
@@ -1,5 +1,12 @@
 #include "include.h"
 
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
