54ffbed92d1891e44a7e202ca08290c2001d92da
ksks2012 <st890751999@gmail.com>
2024-10-21 16:26:09 +0800

    feat(659): Add solution with heap

--- map/659.cpp
+++ map/659.cpp
@@ -24,3 +24,36 @@ public:
         return true;
     }
 };
+
+typedef pair<int,int>PII;
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        priority_queue<PII,vector<PII>,greater<PII>> pq;
+        int n = nums.size();
+        pq.push({nums[0],1});
+
+        for(int i = 1; i < n; i++) {
+            int tmp = nums[i];
+            while(!pq.empty()) {
+                PII t = pq.top();
+                if(t.first < tmp - 1) {
+                    if(t.second < 3) return false;
+                    else pq.pop();
+                } else break;
+            }
+            if(pq.empty() || pq.top().first > tmp - 1) pq.push({tmp,1});
+            else {
+                PII t = pq.top(); pq.pop();
+                pq.push({t.first + 1, t.second + 1});
+            }
+        }
+
+        while(!pq.empty()) {
+            PII t = pq.top(); pq.pop();
+            if(t.second < 3) return false;
+        }
+
+        return true;   
+    }
+};
