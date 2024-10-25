6f0db055e3b616069eb486b3bc66ca36ba1f0fcf
ksks2012 <st890751999@gmail.com>
2024-10-25 19:55:49 +0800

    feat(1233): Add solution with set

--- BFS/1233.cpp
+++ BFS/1233.cpp
@@ -64,12 +64,49 @@ class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(),folder.end());
-        vector<string> result;
+        vector<string> res;
         for(string &s:folder)
-            if(result.empty()                          // Check if 1. This is the first string, 
-                || result.back().compare(0, result.back().length(), s, 0, result.back().length()) != 0 // 2. parent at back is not the parent of `s` by comparing.
-                || s[result.back().length()] != '/')    // 3. If the entire parent matches `s` check if last folder name in parent does not match the folder of same depth in `s`. for cases like `/a/b, /a/bc`.
-                result.push_back(s);                                  
-        return result;
+            if(res.empty()                          // Check if 1. This is the first string, 
+                || res.back().compare(0, res.back().length(), s, 0, res.back().length()) != 0 // 2. parent at back is not the parent of `s` by comparing.
+                || s[res.back().length()] != '/')    // 3. If the entire parent matches `s` check if last folder name in parent does not match the folder of same depth in `s`. for cases like `/a/b, /a/bc`.
+                res.push_back(s);                                  
+        return res;
+    }
+};
+
+// Set
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        unordered_set<string> folderSet(folder.begin(), folder.end());
+        vector<string> res;
+
+        for (string& f : folder) {
+            bool is_sub_folder = false;
+            string prefix = f;
+
+            // Check all prefixes of the current folder path
+            while (!prefix.empty()) {
+                size_t pos = prefix.find_last_of('/');
+                if (pos == string::npos) 
+                    break;
+
+                // Reduce the prefix to its parent folder
+                prefix = prefix.substr(0, pos);
+
+                // If the parent folder exists in the set, mark as sub-folder
+                if (folderSet.count(prefix)) {
+                    is_sub_folder = true;
+                    break;
+                }
+            }
+
+            // If not a sub-folder, add it to the res
+            if (!is_sub_folder) {
+                res.push_back(f);
+            }
+        }
+
+        return res;
     }
 };
