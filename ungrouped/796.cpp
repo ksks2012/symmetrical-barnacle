f758c3bfdde34be2d3cf0317b33649dc0c9c8011
ksks2012 <st890751999@gmail.com>
2024-11-03 22:11:33 +0800

    feat(796): Rotate String
    - Brute force

---
+++ ungrouped/796.cpp
@@ -0,0 +1,18 @@
+#include "../include.h"
+
+// Brute force
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) 
+            return false;
+        int n = s.size();
+
+        for (int rotationCount = 0; rotationCount < n; ++rotationCount) {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if (s == goal) 
+                return true;
+        }
+        return false;
+    }
+};
