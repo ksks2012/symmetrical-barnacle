e9ae499e4e3cb7f1a474aeaf56313a671ddcdf7f
ksks2012 <st890751999@gmail.com>
2024-11-04 16:35:51 +0800

    feat(3163): String Compression III

---
+++ ungrouped/3163.cpp
@@ -0,0 +1,23 @@
+#include "../include.h"
+
+class Solution {
+public:
+    string compressedString(string word) {
+        string res = "";
+        int cnt = 1, n = word.size();
+        char ch = word[0];
+        for(int i = 1; i < n; i++){
+            if(word[i] == ch && cnt < 9) {
+                cnt++;
+            } else{
+                res.push_back(cnt + '0');
+                res.push_back(ch);
+                ch = word[i];
+                cnt = 1;
+            }
+        }
+        res.push_back(cnt + '0');
+        res.push_back(ch);
+        return res;
+    }
+};
