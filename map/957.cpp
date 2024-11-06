be80c8e8cc58a8cfdee69ae20cc97ee685f6b2ad
ksks2012 <st890751999@gmail.com>
2024-11-06 15:43:00 +0800

    feat(957): Add solution that find the loop pattern

--- map/957.cpp
+++ map/957.cpp
@@ -35,3 +35,16 @@ private:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int n) {
+        for (n = (n - 1) % 14 + 1; n > 0; --n) {
+            vector<int> tmp(8, 0);
+            for (int i = 1; i < 7; ++i)
+                tmp[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            cells = tmp;
+        }
+        return cells;
+    }
+};
