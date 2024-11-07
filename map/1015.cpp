067de922c48fcfd93903c75d6cd29f40e3a8c446
ksks2012 <st890751999@gmail.com>
2024-11-07 18:58:21 +0800

    feat(1015): Smallest Integer Divisible by K

---
+++ map/1015.cpp
@@ -0,0 +1,14 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+
+        for(int i = 0, n = 1, n <= k; n++;) {
+            if((i = (i * 10 + 1) % k) == 0) {
+                return n;
+            }
+        }
+        return -1;
+    }
+};
