aba07c0b970f2d18e55f675e46b04bbb2de1e2f0
ksks2012 <st890751999@gmail.com>
2024-11-13 22:12:27 +0800

    style(2563): Update

--- rank/2563.cpp
+++ rank/2563.cpp
@@ -31,8 +31,8 @@ public:
         sort(begin(nums), end(nums));
         long long ans = 0, n = nums.size();
         for(int i = 0; i < n; i++) {
-            int low = lower_bound(begin(nums) + i + 1, end(nums),lower - nums[i]) - begin(nums);
-            int high = upper_bound(begin(nums) + i + 1, end(nums),upper - nums[i]) - begin(nums);
+            int low = lower_bound(begin(nums) + i + 1, end(nums), lower - nums[i]) - begin(nums);
+            int high = upper_bound(begin(nums) + i + 1, end(nums), upper - nums[i]) - begin(nums);
             ans += 1LL * (high - low);
         }
         return ans;
