6a7f8f135358467ead00f31305f4cfd1893df42c
ksks2012 <st890751999@gmail.com>
2025-02-04 16:48:29 +0800

    feat(2529): Update

--- binary_search/2529.cpp
+++ binary_search/2529.cpp
@@ -59,3 +59,20 @@ public:
         return max(it_lower - nums.begin(), nums.end() - it_upper);
     }
 };
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0;
+        int neg = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                pos++;
+            } else if (nums[i] < 0) {
+                neg++;
+            }
+        }
+        return max(pos, neg);
+    }
+};
