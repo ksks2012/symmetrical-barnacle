ce8c2f18d85279edf2b4112bcb91161f3b86caf4
ksks2012 <st890751999@gmail.com>
2025-02-05 15:30:20 +0800

    feat(2300): Add solution

--- binary_search/2300.cpp
+++ binary_search/2300.cpp
@@ -15,3 +15,19 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
+        ranges::sort(potions);
+        int m = spells.size();
+        vector<int> res(m, 0);
+        for (int i = 0; i < m; i++) {
+            long long target = (success - 1) / spells[i];
+            if (target < potions.back()) {
+                res[i] = potions.end() - ranges::upper_bound(potions, (int) target);
+            }
+        }
+        return res;
+    }
+};
