f9e35de2d65a0014e005e26e4166c4a7dd309e41
ksks2012 <st890751999@gmail.com>
2025-02-05 22:56:10 +0800

    style(2389)

--- binary_search/2389.cpp
+++ binary_search/2389.cpp
@@ -8,7 +8,6 @@ public:
         int right = nums.size() - 1;
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            cout << nums[mid] << endl;
             if(nums[mid] == target) {
                 return mid + 1;
             }
