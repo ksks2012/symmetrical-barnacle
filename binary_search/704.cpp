1b0e14bd8f339dbafd2800f31b259e72d44e6191
ksks2012 <st890751999@gmail.com>
2025-02-05 10:28:07 +0800

    feat(704): Optimization

--- binary_search/704.cpp
+++ binary_search/704.cpp
@@ -22,3 +22,26 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
