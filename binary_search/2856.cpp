f09ef3b560490c49b39c9e2a188d20007e0758a1
ksks2012 <st890751999@gmail.com>
2025-02-06 23:51:02 +0800

    feat(2856): Optimization

--- binary_search/2856.cpp
+++ binary_search/2856.cpp
@@ -40,3 +40,22 @@ public:
         return 2 * max_size_num - n;
     }
 };
+
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> tmp(n, 0);
+
+        int m = n - 1;
+
+        unordered_map<int, int> idx_map;
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            idx_map[nums[i]]++;
+            maxi = max(maxi, idx_map[nums[i]]);
+        }
+        
+        return max(maxi * 2 - n, n % 2);
+    }
+};
