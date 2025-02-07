c65dfa6199aa342b44898a849b843ab08de502eb
ksks2012 <st890751999@gmail.com>
2025-02-07 14:41:24 +0800

    feat(1283): Update

--- binary_search/1283.cpp
+++ binary_search/1283.cpp
@@ -22,3 +22,29 @@ public:
         return left;
     }
 };
+
+class Solution {
+public:
+    bool helper(vector<int>& nums, int threshold, int mid) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += (nums[i] + mid - 1) / mid;
+            if(sum > threshold) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int left = 0;
+        int right = 1000000;
+
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            (helper(nums, threshold, mid) ? right : left) = mid;
+        }
+
+        return right;
+    }
+};
