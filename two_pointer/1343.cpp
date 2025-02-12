6d2dd40e3adedfb9d33ca447a840d5bdd819d25e
ksks2012 <st890751999@gmail.com>
2025-02-12 15:23:17 +0800

    fix(1343)

--- two_pointer/1343.cpp
+++ two_pointer/1343.cpp
@@ -3,6 +3,20 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        
+        int res = 0;
+        int tmp = 0;
+        for(int right = 0; right < arr.size(); right++) {
+            tmp += arr[right];
+            if(right < k - 1) {
+                continue;
+            }
+            if(right >= k) {
+                tmp -= arr[right - k];
+            }
+            if(right >= k - 1 && tmp / k >= threshold) {
+                res++;
+            }
+        }
+        return res;
     }
 };
