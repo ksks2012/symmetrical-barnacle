4a5695046a02cfe54689a838655df26129f04720
ksks2012 <st890751999@gmail.com>
2025-02-21 15:54:48 +0800

    feat(2730)

--- two_pointer/2730.cpp
+++ two_pointer/2730.cpp
@@ -27,3 +27,24 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int longestSemiRepetitiveSubstring(string s) {
+        int ans = 1, same = 0, left = 0;
+        for (int right = 1; right < s.length(); right++) {
+            if (s[right] == s[right - 1]) {
+                same++;
+            }
+            if (same > 1) { // same == 2
+                left++;
+                while (s[left] != s[left - 1]) {
+                    left++;
+                }
+                same = 1;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
