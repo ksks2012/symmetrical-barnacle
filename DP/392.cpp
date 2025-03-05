cd4747a845720fd0fb33664cca1a47890175ccf4
ksks2012 <st890751999@gmail.com>
2025-03-05 21:21:49 +0800

    feat(392): Add solution using two pointer

--- DP/392.cpp
+++ DP/392.cpp
@@ -44,3 +44,24 @@ public:
         return dp[m][n] == m;
     }
 };
+
+// Two pointer
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            if(s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return i == m;
+    }
+};
