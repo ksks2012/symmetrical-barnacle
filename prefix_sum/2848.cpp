96f74e54dbe564cc4f0a7d65d99de82946d2a7dc
ksks2012 <st890751999@gmail.com>
2025-04-03 23:45:35 +0800

    feat(update)

--- prefix_sum/2848.cpp
+++ prefix_sum/2848.cpp
@@ -16,3 +16,25 @@ public:
         return tmp.size();
     }
 };
+
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        int max_end = ranges::max(nums, {}, [](const auto& a) { 
+            return a[1]; 
+        })[1];
+
+        vector<int> diff(max_end + 2);
+        for (auto& interval : nums) {
+            diff[interval[0]]++;
+            diff[interval[1] + 1]--;
+        }
+
+        int res = 0, s = 0;
+        for (int d : diff) {
+            s += d;
+            res += s > 0;
+        }
+        return res;
+    }
+};
