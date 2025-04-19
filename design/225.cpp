de9a6e5bc6d9c9ffa2327849766b477c436baff3
ksks2012 <st890751999@gmail.com>
2025-04-19 22:03:40 +0800

    feat(225): Implement Stack using Queues

---
+++ design/225.cpp
@@ -0,0 +1,31 @@
+#include "include.h"
+
+class MyStack {
+public:
+    queue<int> que;
+    // Push element x onto stack.
+    void push(int x) {
+        que.push(x);
+        for(int i=0;i<que.size()-1;++i){
+            que.push(que.front());
+            que.pop();
+        }
+    }
+
+    // Removes the element on top of the stack.
+    int pop() {
+        int tmp = que.front();
+        que.pop();
+        return tmp;
+    }
+
+    // Get the top element.
+    int top() {
+        return que.front();
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return que.empty();
+    }
+};
