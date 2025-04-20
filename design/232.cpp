7de0a66c3f7514b4429b4ebe7b7608da106b1766
ksks2012 <st890751999@gmail.com>
2025-04-20 21:51:13 +0800

    feat(232): update

--- design/232.cpp
+++ design/232.cpp
@@ -39,3 +39,28 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+
+ class Queue {
+    stack<int> input, output;
+public:
+
+    void push(int x) {
+        input.push(x);
+    }
+
+    void pop(void) {
+        peek();
+        output.pop();
+    }
+
+    int peek(void) {
+        if (output.empty())
+            while (input.size())
+                output.push(input.top()), input.pop();
+        return output.top();
+    }
+
+    bool empty(void) {
+        return input.empty() && output.empty();
+    }
+};
