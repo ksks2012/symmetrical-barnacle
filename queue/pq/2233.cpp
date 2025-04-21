8c4a27d78c40479ba53c1986bbf370b289c8dce1
ksks2012 <st890751999@gmail.com>
2025-04-21 18:52:20 +0800

    feat(2233): Maximum Product After K Increments

---
+++ queue/pq/2233.cpp
@@ -0,0 +1,17 @@
+#include "include.h"
+
+constexpr int P = 1000000007;
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        make_heap(begin(nums), end(nums), greater<int>());
+        while (k--) {
+            pop_heap(begin(nums), end(nums), greater<int>());
+            ++nums.back();
+            push_heap(begin(nums), end(nums), greater<int>());
+        }
+        return accumulate(begin(nums), end(nums), 1LL, [](long long res, int n) { 
+            return res * n % P; 
+        });
+    }
+};
