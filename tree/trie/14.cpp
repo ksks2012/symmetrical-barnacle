4746ef59b8fcb385e4f1468d862b79836a2939fd
ksks2012 <st890751999@gmail.com>
2025-04-26 21:10:45 +0800

    feat(14): Update solution with trie

--- tree/trie/14.cpp
+++ tree/trie/14.cpp
@@ -1,5 +1,72 @@
 #include "include.h"
 
+// Using Trie to solve the problem
+class TrieNode {
+public:
+    TrieNode* children[26];
+    int count;
+
+    TrieNode() {
+        count = 0;
+        for (int i = 0; i < 26; ++i)
+            children[i] = nullptr;
+    }
+};
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                node->children[idx] = new TrieNode();
+            node = node->children[idx];
+            node->count++;
+        }
+    }
+
+    string getCommonPrefix(int totalWords) {
+        string prefix;
+        TrieNode* node = root;
+        while (true) {
+            int next = -1;
+            for (int i = 0; i < 26; ++i) {
+                if (node->children[i]) {
+                    if (node->children[i]->count == totalWords) {
+                        next = i;
+                        break;
+                    } else {
+                        return prefix;
+                    }
+                }
+            }
+            if (next == -1) break;
+            prefix += (char)(next + 'a');
+            node = node->children[next];
+        }
+        return prefix;
+    }
+};
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        Trie trie;
+        for (const string& word : strs) {
+            trie.insert(word);
+        }
+
+        return trie.getCommonPrefix(strs.size());
+    }
+};
+    
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
