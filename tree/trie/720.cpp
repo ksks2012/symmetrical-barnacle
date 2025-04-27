022052fb4d1ab34350ec67c2e0557224eaf12588
ksks2012 <st890751999@gmail.com>
2025-04-27 23:10:20 +0800

    feat(720): Add solution with trie

--- tree/trie/720.cpp
+++ tree/trie/720.cpp
@@ -27,3 +27,73 @@ public:
         return res;
     }
 };
+
+// Trie
+class TrieNode{
+public:
+    unordered_map<char, TrieNode*> children;
+    bool isWord = false;
+    char val;
+    
+    TrieNode(){}
+    
+    TrieNode(char v){ this->val = v; }
+};
+
+class Trie{
+private:
+    TrieNode* root;
+public:
+    Trie(){
+        root = new TrieNode();
+    }
+    
+    void insert(string word){
+        TrieNode* temp = root;
+        
+        for(char ch : word){
+            if(temp->children.find(ch) == temp->children.end()){
+                temp->children[ch] = new TrieNode(ch);
+            }
+            temp = temp->children[ch];
+        }
+        
+        temp->isWord = true;
+    }
+    
+    bool isOrdered(string word){
+        TrieNode* temp = root;
+        
+        for(char ch : word){
+            if(temp == root || temp->isWord){
+                temp = temp->children[ch];
+                continue;
+            }
+            return false;
+        }
+        
+        return true;
+    }
+};
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        Trie* search = new Trie();
+        string res = "";
+        int size = 0;
+        
+        for(string str : words){
+            search->insert(str);
+        }
+        
+        for(string str : words){
+            if(search->isOrdered(str) && size <= str.size()){
+                res = (size == str.size()) ? min(res, str) : str;
+                size = str.size();
+            }
+        }
+        
+        return res;
+    }
+};
