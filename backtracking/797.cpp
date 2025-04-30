afc33ff6f0453e642d2f3767948048cb4af880ec
ksks2012 <st890751999@gmail.com>
2025-05-01 00:18:57 +0800

    feat(797): Update

--- backtracking/797.cpp
+++ backtracking/797.cpp
@@ -22,3 +22,26 @@ public:
         }
     }
 };
+
+// lee
+class Solution {
+public:
+    void DFS(vector<vector<int>>& graph, vector<vector<int>>& res, vector<int>& path, int cur) {
+        path.push_back(cur);
+        if (cur == graph.size() - 1)
+            res.push_back(path);
+        else for (auto it: graph[cur])
+            DFS(graph, res, path, it);
+        path.pop_back();
+
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> res;
+        vector<int> path;
+        DFS(graph, res, path, 0);
+        return res;
+    }
+};
+
+   
