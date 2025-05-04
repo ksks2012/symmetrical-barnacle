cd29a646b4dfc36e58e24d3df44f0ae0d51dfaf6
ksks2012 <st890751999@gmail.com>
2025-05-04 23:06:24 +0800

    feat(890): Find and Replace Pattern

---
+++ map/890.cpp
@@ -0,0 +1,35 @@
+#include "include.h"
+
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> res;
+        
+        for(auto word : words) {
+            unordered_map<char, char> map;
+            unordered_map<char, char> map2;
+            bool check = true;
+            
+            for(int i = 0; i < word.size(); i++) {
+                if(map.find(word[i]) == map.end()) {
+                    map[word[i]] = pattern[i];
+                } else if(map[word[i]] != pattern[i]) {
+                    check = false;
+                    break;
+                }
+                
+                if(map2.find(pattern[i]) == map2.end()) {
+                    map2[pattern[i]] = word[i];
+                } else if(map2[pattern[i]] != word[i]) {
+                    check = false;
+                    break;
+                }
+            }
+            
+            if(check) 
+                res.push_back(word);
+        }
+        
+        return res;
+    }
+};
