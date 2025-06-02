6407b14d6e71c622a938118cce35eca495a2c87a
ksks2012 <st890751999@gmail.com>
2025-06-02 23:14:59 +0800

    feat(846): Update

--- map/846.cpp
+++ map/846.cpp
@@ -26,3 +26,20 @@ public:
         return true;   
     }
 };
+
+// lee
+class Solution {
+public:
+    bool isNStraightHand(vector<int> hand, int groupSize) {
+        map<int, int> mp;
+        for (int i : hand) 
+            mp[i]++;
+            
+        for (auto it : mp)
+            if (mp[it.first] > 0)
+                for (int i = groupSize - 1; i >= 0; --i)
+                    if ((mp[it.first + i] -= mp[it.first]) < 0)
+                        return false;
+        return true;
+    }
+};
