42f20a29940a27708024c093035b9f441a4e5a6a
ksks2012 <st890751999@gmail.com>
2025-06-04 22:41:34 +0800

    feat(1700): Simulation

--- queue/1700.cpp
+++ queue/1700.cpp
@@ -21,3 +21,34 @@ public:
         return n - i;
     }
 };
+
+
+// simulation
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> q;
+        for (int s : students) {
+            q.push(s);
+        }
+
+        int index = 0;             // pointer for sandwiches
+        int tries = 0;         
+
+        while (!q.empty() && tries < q.size()) {
+            if (q.front() == sandwiches[index]) {
+                // Student eats, remove from queue, move to next sandwich
+                q.pop();
+                index++;
+                tries = 0; // Reset tries counter
+            } else {
+                // Student refuses, move to back of queue
+                q.push(q.front());
+                q.pop();
+                tries++; // Record one refusal
+            }
+        }
+
+        return q.size(); // Number of students who couldn't eat
+    }
+};
