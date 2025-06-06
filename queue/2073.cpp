f980b11d4b53ca27da1d281b68098c3932febe07
ksks2012 <st890751999@gmail.com>
2025-06-06 22:32:03 +0800

    feat(2073): update

--- queue/2073.cpp
+++ queue/2073.cpp
@@ -29,3 +29,13 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+   int timeRequiredToBuy(vector<int>& t, int k) {
+        int res = 0;
+        for (int i = 0; i < t.size(); ++i)
+            res += min(t[k] - (i > k), t[i]);
+        return res;
+    }
+};
