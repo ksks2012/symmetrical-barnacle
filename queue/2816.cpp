4c77dfd09bf9da2beeca27cbc0c87e414cc5f8d8
ksks2012 <st890751999@gmail.com>
2025-06-08 20:37:00 +0800

    feat(2816): Update

--- queue/2816.cpp
+++ queue/2816.cpp
@@ -37,3 +37,17 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        if (head->val > 4)
+            head = new ListNode(0, head);
+        for(ListNode* node = head; node; node = node->next) {
+            node->val = (node->val * 2) % 10;
+            if (node->next && node->next->val > 4)
+                node->val++;
+        }
+        return head;
+    }
+};
