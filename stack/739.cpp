540fc5f7e14719d16dc65e7212e418deb039a118
ksks2012 <st890751999@gmail.com>
2025-06-11 22:58:42 +0800

    feat(739): Update

--- stack/739.cpp
+++ stack/739.cpp
@@ -18,3 +18,25 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temper) {
+        int n = temper.size();
+        vector<int> st;
+        vector<int> res(n, 0);
+        
+        // [ 0, 1, 2, 3, 4, 5, 6, 7]
+        // [73,74,75,71,69,72,76,73]
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && temper[st.back()] <= temper[i]) {
+                st.pop_back();
+            }
+            if(!st.empty()) {
+                res[i] = st.back() - i;
+            }
+            st.push_back(i);
+        }
+        return res;
+    }
+};
