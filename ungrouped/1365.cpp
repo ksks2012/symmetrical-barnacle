1dac0273736146659ff3d2adaa99f8e8b7d3287d
ksks2012 <st890751999@gmail.com>
2025-06-22 21:30:14 +0800

    feat(1365): Update

--- ungrouped/1365.cpp
+++ ungrouped/1365.cpp
@@ -20,3 +20,29 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+  
+        int n = nums.size();
+        vector<int> res(n, 0);
+
+        vector<int> tmp(101, 0);
+        for (int num : nums) {
+            tmp[num]++;
+        }
+        for (int i = 1; i < 101; i++) {
+            tmp[i] += tmp[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                res[i] = 0;
+            } else {
+                res[i] = tmp[nums[i] - 1];
+            }
+        }
+
+        return res;
+    }
+};
