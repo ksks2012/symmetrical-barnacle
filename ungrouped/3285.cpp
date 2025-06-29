92c086c3f2d93034dd454eeeda53c0bfd615a2d5
ksks2012 <st890751999@gmail.com>
2025-06-29 11:16:57 +0800

    feat(3285): Find Indices of Stable Mountains

---
+++ ungrouped/3285.cpp
@@ -0,0 +1,17 @@
+#include "../include.h"
+ 
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        int n = height.size();
+        vector<int> res;
+        
+        for(int i = 1; i < n; i++) {
+            if(height[i - 1] > threshold) {
+                res.push_back(i);
+            }
+        }
+
+        return res;
+    }
+};
