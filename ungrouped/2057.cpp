9a3c555f293bc775b9edd544fe7f820dc4e87a46
ksks2012 <st890751999@gmail.com>
2025-07-02 14:45:55 +0800

    feat(2057): Smallest Index With Equal Value

---
+++ ungrouped/2057.cpp
@@ -0,0 +1,14 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i % 10 == nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
