a8e58840706438ca2f1f252940f4f198bcf0e6c1
ksks2012 <st890751999@gmail.com>
2025-07-09 18:19:21 +0800

    feat(905): Sort Array By Parity

---
+++ ungrouped/905.cpp
@@ -0,0 +1,15 @@
+#include "../include.h"
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0, j = 0; j < n; j++) {
+            if(nums[j] % 2 == 0) {
+                swap(nums[i++], nums[j]);
+            }
+        }
+
+        return nums;
+    }
+};
