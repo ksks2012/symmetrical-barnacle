189d4841fd384895eaf8f8be6094f10081a1319b
ksks2012 <st890751999@gmail.com>
2025-07-20 15:13:54 +0800

    feat(852): Update

--- binary_search/852.cpp
+++ binary_search/852.cpp
@@ -18,3 +18,32 @@ public:
         return left;
     }
 };
+
+// lee
+// Golden section search version
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        auto gold1 = [](int l, int r) {
+            return l + int(round((r - l) * 0.382));
+        };
+        auto gold2 = [](int l, int r) {
+            return l + int(round((r - l) * 0.618));
+        };
+        int l = 0, r = arr.size() - 1;
+        int x1 = gold1(l, r), x2 = gold2(l, r);
+        while (x1 < x2) {
+            if (arr[x1] < arr[x2]) {
+                l = x1;
+                x1 = x2;
+                x2 = gold1(x1, r);
+            } else {
+                r = x2;
+                x2 = x1;
+                x1 = gold2(l, x2);
+            }
+        }
+        auto it = max_element(arr.begin() + l, arr.begin() + r + 1);
+        return distance(arr.begin(), it);
+    }
+};
