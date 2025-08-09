676b3c15dab2686b6f070eb7183f77410a51ee74
ksks2012 <st890751999@gmail.com>
2025-08-09 17:40:10 +0800

    feat(2418): Update

--- ungrouped/2418.cpp
+++ ungrouped/2418.cpp
@@ -16,3 +16,22 @@ public:
         return res;
     }
 };
+
+// kiran: This code sorts a list of people by their heights in descending order and returns their names in that order.
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+            vector<int> idx(names.size());
+            for (int i = 0; i < names.size(); ++i) {
+                idx[i] = i;
+            }
+            sort(idx.begin(), idx.end(), [&](int i, int j) {
+                return heights[i] > heights[j];
+            });
+            vector<string> ans(names.size());
+            for (int i = 0; i < names.size(); ++i) {
+                ans[i] = names[idx[i]];
+            }
+            return ans;
+        }
+};
