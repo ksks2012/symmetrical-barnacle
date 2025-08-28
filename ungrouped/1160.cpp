32dbc19c27f26f3289b97ca4ef8ee3d0c4fc11ea
ksks2012 <st890751999@gmail.com>
2025-08-28 21:37:00 +0800

    feat(1160): Find Words That Can Be Formed by Characters

---
+++ ungrouped/1160.cpp
@@ -0,0 +1,33 @@
+#include "include.h"
+
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map<char, int> charCount;
+        for (char c : chars) {
+            charCount[c]++;
+        }
+
+        int totalLength = 0;
+        for (const string& word : words) {
+            unordered_map<char, int> wordCount;
+            for (char c : word) {
+                wordCount[c]++;
+            }
+
+            bool canForm = true;
+            for (const auto& [c, count] : wordCount) {
+                if (charCount[c] < count) {
+                    canForm = false;
+                    break;
+                }
+            }
+
+            if (canForm) {
+                totalLength += word.length();
+            }
+        }
+
+        return totalLength;
+    }
+};
