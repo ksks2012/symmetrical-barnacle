62306ab12499018c91d0ba4710826ea61ca14fc0
ksks2012 <st890751999@gmail.com>
2025-09-12 17:13:46 +0800

    feat(2656): Maximum Sum With Exactly K Elements

---
+++ ungrouped/2656.cpp
@@ -0,0 +1,13 @@
+#include "include.h"
+
+class Solution {
+public:
+    int maximizeSum(vector<int>& nums, int k) {
+        int maxi = *max_element(nums.begin(), nums.end());
+        int sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += maxi + i;
+        }
+        return sum;
+    }
+};
