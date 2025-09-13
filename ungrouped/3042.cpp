c3c5e874b71a933cf9f3dad9aaff8318589288ac
ksks2012 <st890751999@gmail.com>
2025-09-13 13:40:06 +0800

    feat(3042): Update trie solution

--- ungrouped/3042.cpp
+++ ungrouped/3042.cpp
@@ -24,3 +24,40 @@ public:
         return ans;
     }
 };
+
+// Trie solution
+struct TrieNode {
+    TrieNode* next[26];
+    int count = 0;
+
+    TrieNode() { 
+        fill(next, next+26, (TrieNode*)NULL); 
+    }
+};
+
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        TrieNode root = TrieNode();
+        int ans = 0;
+        for (string& s : words) {
+            TrieNode* Node = &root;
+            int n = s.size();
+            for (int i = 0; i < n; i++) {
+                // "ac"->(ac)(ca) "acbac"->(ac)(ca)(bb)(ac)(ca)
+                int index = s[i] - 'a';
+                if (Node->next[index] == 0)
+                    Node->next[index] = new TrieNode();
+                Node = Node->next[index];
+
+                index = s[n - 1 - i] - 'a';
+                if (Node->next[index] == 0)
+                    Node->next[index] = new TrieNode();
+                Node = Node->next[index];
+                ans += Node->count;
+            }
+            Node->count++;
+        }
+        return ans;
+    }
+};
