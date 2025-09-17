6ecf1bc4294513703a89658c658ef9808867ce9f
ksks2012 <st890751999@gmail.com>
2025-09-17 10:01:22 +0800

    feat(2108): Find First Palindromic String in the Array

---
+++ ungrouped/2108.cpp
@@ -0,0 +1,25 @@
+#include "include.h"
+
+class Solution {
+public:
+    bool isPalindrome(const string& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    string firstPalindrome(vector<string>& words) {
+        for (const string& word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
