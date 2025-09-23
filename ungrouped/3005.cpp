5ce366ee7afaa0ec3aa64a1c850d540cbb9932c3
ksks2012 <st890751999@gmail.com>
2025-09-23 16:35:08 +0800

    feat(3005): Update

--- ungrouped/3005.cpp
+++ ungrouped/3005.cpp
@@ -5,7 +5,7 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int, int> m;
         int maxi = 0;
-        int ans = 0;
+        int res = 0;
         for(int i = 0; i < nums.size(); i++) {
             m[nums[i]]++;
             maxi = max(maxi, m[nums[i]]);
@@ -13,9 +13,9 @@ public:
 
         for(auto i : m) {
             if(i.second == maxi)
-                ans += maxi;
+                res += maxi;
         }
 
-        return ans;
+        return res;
     }
 };
