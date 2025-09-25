159c28f8f460654d44a1443055e1c1ba14888115
ksks2012 <st890751999@gmail.com>
2025-09-25 23:03:19 +0800

    feat(1662): Check If Two String Arrays are Equivalent

---
+++ ungrouped/1662.cpp
@@ -0,0 +1,13 @@
+#include "include.h"
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "", s2 = "";
+        for (const auto& w : word1) 
+            s1 += w;
+        for (const auto& w : word2) 
+            s2 += w;
+        return s1 == s2;
+    }
+};
