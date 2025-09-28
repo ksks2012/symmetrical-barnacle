dd94e28e9611e71539cd68eb3667f4809b2bfe8d
ksks2012 <st890751999@gmail.com>
2025-09-28 23:46:40 +0800

    feat(2859): Update

--- ungrouped/2859.cpp
+++ ungrouped/2859.cpp
@@ -11,3 +11,20 @@ public:
         return res;
     }
 };
+
+// hacked by Vlad
+class Solution {
+public:
+    int getNextSamePopcount(int a) {
+        if (a == 0)
+            return INT_MAX;
+        int c = (a & -a), r = a + c;
+        return ((((r ^ a) >> 2) / c) | r);
+    }    
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int res = 0;
+        for (int i = (1 << k) - 1; i < nums.size(); i = getNextSamePopcount(i))
+            res += nums[i];
+        return res;
+    }
+};
