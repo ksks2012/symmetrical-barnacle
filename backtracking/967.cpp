c356cb9ccbea59b1961eb3ea10dff6d892e44274
ksks2012 <st890751999@gmail.com>
2025-11-03 00:07:36 +0800

    feat(967): Update

--- backtracking/967.cpp
+++ backtracking/967.cpp
@@ -27,3 +27,36 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    void dfs(vector<int>& res, int remaining, int k, int cur) {
+        if (remaining == 0) {
+            res.push_back(cur);
+            return;
+        }
+        int last = cur % 10;
+        int up = last + k;
+        if (up < 10) 
+            dfs(res, remaining - 1, k, cur * 10 + up);
+        if (k != 0) {
+            int down = last - k;
+            if (down >= 0) 
+                dfs(res, remaining - 1, k, cur * 10 + down);
+        }
+    }
+
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> res;
+        if (n == 1) { // include 0 when n == 1
+            res.reserve(10);
+            for (int i = 0; i <= 9; ++i) 
+                res.push_back(i);
+            return res;
+        }
+        res.reserve(9 << min(n - 1, 7)); // heuristic reserve to reduce reallocs
+        for (int i = 1; i <= 9; ++i)
+            dfs(res, n - 1, k, i);
+        return res;
+    }
+};
