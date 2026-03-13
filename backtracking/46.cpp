d6cb845c1257aaebdc7c453bbfe628e1cab36701
ksks2012 <st890751999@gmail.com>
2026-03-13 18:04:45 +0800

    feat(46): Permutations

---
+++ backtracking/46.cpp
@@ -0,0 +1,26 @@
+#include "include.h"
+
+class Solution {
+public:
+    void back_tracking(vector<vector<int>>& res, vector<int>& nums, int cur) {
+        if (cur == nums.size() -1) {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = cur; i < nums.size(); i++) {
+            swap(nums[i], nums[cur]);
+            back_tracking(res, nums, cur + 1);
+            swap(nums[cur], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+
+        if (nums.empty()) return res;
+        back_tracking(res, nums, 0);
+
+        return res;
+    }
+
+};
