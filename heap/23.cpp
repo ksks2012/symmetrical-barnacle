1353a447a16baf0de60031d93262202cc6ffdc9e
ksks2012 <st890751999@gmail.com>
2026-03-15 15:46:09 +0800

    feat(23): Update

--- heap/23.cpp
+++ heap/23.cpp
@@ -43,3 +43,38 @@ public:
         return lists.front();
     }
 };
+
+// Heap
+class Solution {
+public:
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;  // min-heap
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
+        
+        for (auto node : lists) {
+            if (node) minHeap.push(node);
+        }
+        
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (!minHeap.empty()) {
+            ListNode* cur = minHeap.top();
+            minHeap.pop();
+            
+            tail->next = cur;
+            tail = tail->next;
+            
+            if (cur->next) {
+                minHeap.push(cur->next);
+            }
+        }
+        
+        return dummy.next;
+    }
+};
