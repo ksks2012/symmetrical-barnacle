03be3d479239c2c237c5c0a4cc8a6832e5818dfc
ksks2012 <st890751999@gmail.com>
2026-03-15 15:07:28 +0800

    feat(206): Reverse Linked List

---
+++ linked_list/206.cpp
@@ -0,0 +1,29 @@
+#include "include.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* ptr, *pre;
+
+        ptr = head;
+        pre = nullptr;
+
+        while(ptr != nullptr) {
+            ListNode* tmp = ptr->next;
+            ptr->next = pre;
+            pre = ptr;
+            ptr = tmp;
+        }
+        return pre;
+    }
+};
