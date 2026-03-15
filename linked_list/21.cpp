2cae0a64f916f4c0d376bfe7485f767f3183d817
ksks2012 <st890751999@gmail.com>
2026-03-15 15:29:19 +0800

    feat(21): Update

--- linked_list/21.cpp
+++ linked_list/21.cpp
@@ -25,3 +25,30 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) return list2;
+        if (!list2) return list1;
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+
+        // Append any remaining elements from either list
+        tail->next = list1 ? list1 : list2;
+
+        return dummy->next;
+    }
+};
