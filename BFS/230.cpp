233cfec40485366d56499bd2f4955876b3ef3c3b
ksks2012 <st890751999@gmail.com>
2026-03-16 22:35:50 +0800

    feat(230): Update

--- BFS/230.cpp
+++ BFS/230.cpp
@@ -42,3 +42,29 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        int count = 0;
+        
+        while (curr || !st.empty()) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            
+            curr = st.top();
+            st.pop();
+            
+            if (++count == k) {
+                return curr->val;
+            }
+            
+            curr = curr->right;
+        }
+        return -1; // fail
+    }
+};
