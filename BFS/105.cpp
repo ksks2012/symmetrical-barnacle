3b2668081892c9b45731fb98a22e837fa8eb74ca
ksks2012 <st890751999@gmail.com>
2026-03-18 00:33:04 +0800

    feat(105): Update

--- BFS/105.cpp
+++ BFS/105.cpp
@@ -26,3 +26,50 @@ public:
         return cur;
     }
 };
+
+class Solution {
+public:
+    // Used for fast lookup of value -> inorder index mapping
+    unordered_map<int, int> inorderIndex;
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        // Build hash map in advance: value -> index in inorder array
+        for (int i = 0; i < inorder.size(); ++i) {
+            inorderIndex[inorder[i]] = i;
+        }
+        
+        return build(preorder, 0, preorder.size() - 1,
+                     inorder,   0, inorder.size() - 1);
+    }
+    
+private:
+    TreeNode* build(vector<int>& preorder, int preL, int preR,
+                    vector<int>& inorder,  int inL,  int inR) {
+        // Invalid range, return null
+        if (preL > preR || inL > inR) {
+            return nullptr;
+        }
+        
+        // First element of preorder is root of current subtree
+        int rootVal = preorder[preL];
+        
+        // Find root's index in inorder using hash map in O(1)
+        int rootInIndex = inorderIndex[rootVal];
+        
+        // Compute size of left subtree (number of nodes)
+        int leftSize = rootInIndex - inL;
+        
+        // Create current node
+        TreeNode* root = new TreeNode(rootVal);
+        
+        // Recursively build left subtree
+        root->left = build(preorder, preL + 1,           preL + leftSize,
+                           inorder,   inL,               rootInIndex - 1);
+        
+        // Recursively build right subtree
+        root->right = build(preorder, preL + leftSize + 1, preR,
+                            inorder,   rootInIndex + 1,    inR);
+        
+        return root;
+    }
+};
