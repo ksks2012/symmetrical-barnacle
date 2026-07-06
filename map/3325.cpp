bab99bac155ed78d0798527a584d026b1e795bad
ksks2012 <st890751999@gmail.com>
2026-07-06 10:44:41 +0800

    feat(3325): Update

--- map/3325.cpp
+++ map/3325.cpp
@@ -14,3 +14,19 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        int res = 0, left = 0, cnt[26]{};
+        for (char c : s) {
+            cnt[c - 'a']++;
+            while (cnt[c - 'a'] >= k) {
+                cnt[s[left] - 'a']--;
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+};
